f6ba66ce0f2a746061532e0b7ff58e0b0a4e5573
Pedro Camargo <veigacamargo@gmail.com>
2016-08-21 21:51:01 +1000

    Creating distribution module for future model feature

---
+++ aequilibrae/distribution/__init__.py
@@ -0,0 +1,16 @@
+"""
+=============
+Trip distribution code
+=============
+
+STILL NEED TO ADD SOME EXPLANATIONS HERE
+
+"""
+
+__author__ = "Pedro Camargo ($Author: Pedro Camargo $)"
+__version__ = "1.0"
+__revision__ = "$Revision: 1 $"
+__date__ = "$Date: 2016-07-02$"
+
+#from . import AssignmentResults
+#from . import PathResults
