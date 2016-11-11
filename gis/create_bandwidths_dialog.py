8a5aaa0224eb1b2f0005973a47759bf99d9b6912
veigacamargo@gmail.com <nUSP3126170>
2016-11-11 20:27:57 +1000

    Improved interface and functionality of the stacked bandwidths feature

--- gis/create_bandwidths_dialog.py
+++ gis/create_bandwidths_dialog.py
@@ -20,6 +20,7 @@
  """
 
 import qgis
+from functools import partial
 from qgis.core import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -52,6 +53,9 @@ class CreateBandwidthsDialog(QDialog, Ui_bandwidths):
         self.mMapLayerComboBox.setFilters(QgsMapLayerProxyModel.LineLayer)
         self.mMapLayerComboBox.layerChanged.connect(self.add_fields_to_cboxes)
 
+        self.ab_FieldComboBox.currentIndexChanged.connect(partial(self.choose_a_field, 'AB'))
+        self.ba_FieldComboBox.currentIndexChanged.connect(partial(self.choose_a_field, 'BA'))
+
         # space slider
         self.slider_spacer.setMinimum(1)
         self.slider_spacer.setMaximum(10)
@@ -69,18 +73,53 @@ class CreateBandwidthsDialog(QDialog, Ui_bandwidths):
         self.slider_band_size.valueChanged.connect(self.sizevaluechange)
 
         #List of bands
-        self.bands_list.setColumnWidth(0, 230)
-        self.bands_list.setColumnWidth(1, 230)
-        self.bands_list.setColumnWidth(2, 80)
+        self.bands_list.setColumnWidth(0, 210)
+        self.bands_list.setColumnWidth(1, 210)
+        self.bands_list.setColumnWidth(2, 110)
+        self.bands_list.setColumnWidth(3, 110)
+
+
         self.but_add_band.clicked.connect(self.add_to_bands_list)
-        self.bands_list.doubleClicked.connect(self.slot_double_clicked)
+        self.bands_list.setEditTriggers(QTableWidget.NoEditTriggers)
+
+        # self.bands_list.doubleClicked.connect(self.slot_double_clicked)
 
         self.but_run.clicked.connect(self.add_bands_to_map)
         self.add_fields_to_cboxes()
         self.random_rgb()
         self.sizevaluechange()
         self.spacevaluechange()
+        self.set_initial_value_if_available()
 
+    def choose_a_field(self, modified):
+        i, j = 'AB', 'BA'
+
+        if modified == i:
+            text = self.ab_FieldComboBox.currentText()
+            if i in text:
+                text = text.replace(i, j)
+                index = self.ba_FieldComboBox.findText(text, Qt.MatchFixedString)
+                if index >= 0:
+                    self.ba_FieldComboBox.setCurrentIndex(index)
+
+        if modified == j:
+            text = self.ba_FieldComboBox.currentText()
+            if j in text:
+                text = text.replace(j, i)
+                index = self.ab_FieldComboBox.findText(text, Qt.MatchFixedString)
+                if index >= 0:
+                    self.ab_FieldComboBox.setCurrentIndex(index)
+
+
+    def set_initial_value_if_available(self):
+        all_items = [self.ab_FieldComboBox.itemText(i) for i in range(self.ab_FieldComboBox.count())]
+
+        for i in all_items:
+            if 'AB' in i:
+                index = self.ab_FieldComboBox.findText(i, Qt.MatchFixedString)
+                if index >= 0:
+                    self.ab_FieldComboBox.setCurrentIndex(index)
+                break
 
     def spacevaluechange(self):
         self.space_size = self.slider_spacer.value() / 100.0
@@ -90,11 +129,12 @@ class CreateBandwidthsDialog(QDialog, Ui_bandwidths):
         self.band_size = self.slider_band_size.value() / 5.0
         self.lbl_width.setText("{:3,.2f}".format(self.band_size))
 
-    def slot_double_clicked(self, mi):
-        row = mi.row()
-        if row > -1:
-            self.bands_list.removeRow(row)
-            self.tot_bands -= 1
+    # # Method for removing a line from the table with a double click
+    # def slot_double_clicked(self, mi):
+    #     row = mi.row()
+    #     if row > -1:
+    #         self.bands_list.removeRow(row)
+    #         self.tot_bands -= 1
 
     def add_fields_to_cboxes(self):
         self.layer = get_vector_layer_by_name(self.mMapLayerComboBox.currentText())
@@ -106,15 +146,99 @@ class CreateBandwidthsDialog(QDialog, Ui_bandwidths):
             ab_band = self.layer.fieldNameIndex(self.ab_FieldComboBox.currentText())
             ba_band = self.layer.fieldNameIndex(self.ba_FieldComboBox.currentText())
 
+            self.bands_list.setRowCount(self.tot_bands + 1)
+
+            # Field names
+            self.bands_list.setItem(self.tot_bands, 0, QTableWidgetItem(self.ab_FieldComboBox.currentText()))
+            self.bands_list.setItem(self.tot_bands, 1, QTableWidgetItem(self.ba_FieldComboBox.currentText()))
+
+            # color
+            self.bands_list.setItem(self.tot_bands, 2, QTableWidgetItem(''))
+            self.bands_list.item(self.tot_bands, 2).setBackground(self.mColorButton.color())
+
+
+            # Up-Down buttons
+
+            button_up = QToolButton()
+            button_up.setArrowType(Qt.UpArrow)
+            button_up.clicked.connect(self.click_button_inside_the_list)
+
+            button_down = QToolButton()
+            button_down.setArrowType(Qt.DownArrow)
+            button_down.clicked.connect(self.click_button_inside_the_list)
+
+            del_button = QPushButton('X')
+            del_button.clicked.connect(self.click_button_inside_the_list)
+
+            layout = QHBoxLayout()
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.setSpacing(0)
+            layout.addWidget(button_down)
+            layout.addWidget(button_up)
+            layout.addWidget(del_button)
+
+            cellWidget = QWidget()
+            cellWidget.setLayout(layout)
+            self.bands_list.setCellWidget(self.tot_bands, 3, cellWidget)
+
+            # incrementing and moving on
             self.tot_bands += 1
-            self.bands_list.setRowCount(self.tot_bands)
-
-            self.bands_list.setItem(self.tot_bands - 1, 0, QTableWidgetItem(self.ab_FieldComboBox.currentText()))
-            self.bands_list.setItem(self.tot_bands - 1, 1, QTableWidgetItem(self.ba_FieldComboBox.currentText()))
-            self.bands_list.setItem(self.tot_bands - 1, 2, QTableWidgetItem(''))
-
-            self.bands_list.item(self.tot_bands - 1, 2).setBackground(self.mColorButton.color())
             self.random_rgb()
+
+    def click_button_inside_the_list(self):
+        button = self.sender()
+        parent = button.parent()
+
+        for i in range(self.tot_bands):
+            if self.bands_list.cellWidget(i,3) is parent:
+                row = i
+                break
+
+        column = 2
+        for i, q in enumerate(button.parentWidget().findChildren(QToolButton)):
+            if q is button:
+                column = i
+
+
+        # if we clicked the "remove button"
+        if column == 0 and row < self.tot_bands -1:
+            low_cl = self.bands_list.item(row + 1, 2).backgroundColor()
+            low_ab = self.bands_list.item(row + 1, 0).text()
+            low_ba = self.bands_list.item(row + 1, 1).text()
+
+            top_cl = self.bands_list.item(row, 2).backgroundColor()
+            top_ab = self.bands_list.item(row, 0).text()
+            top_ba = self.bands_list.item(row, 1).text()
+
+
+            self.bands_list.setItem(row, 0, QTableWidgetItem(low_ab))
+            self.bands_list.setItem(row, 1, QTableWidgetItem(low_ba))
+            self.bands_list.item(row, 2).setBackground(low_cl)
+
+            self.bands_list.setItem(row + 1, 0, QTableWidgetItem(top_ab))
+            self.bands_list.setItem(row + 1, 1, QTableWidgetItem(top_ba))
+            self.bands_list.item(row + 1, 2).setBackground(top_cl)
+
+        elif column == 1 and row > 0:
+            low_cl = self.bands_list.item(row, 2).backgroundColor()
+            low_ab = self.bands_list.item(row, 0).text()
+            low_ba = self.bands_list.item(row, 1).text()
+
+            top_cl = self.bands_list.item(row - 1, 2).backgroundColor()
+            top_ab = self.bands_list.item(row - 1, 0).text()
+            top_ba = self.bands_list.item(row - 1, 1).text()
+
+            self.bands_list.setItem(row, 0, QTableWidgetItem(top_ab))
+            self.bands_list.setItem(row, 1, QTableWidgetItem(top_ba))
+            self.bands_list.item(row, 2).setBackground(top_cl)
+
+            self.bands_list.setItem(row -1, 0, QTableWidgetItem(low_ab))
+            self.bands_list.setItem(row -1, 1, QTableWidgetItem(low_ba))
+            self.bands_list.item(row - 1, 2).setBackground(low_cl)
+
+        elif column == 2:
+            self.bands_list.removeRow(row)
+            self.tot_bands -= 1
 
     def random_rgb(self):
         rgb = []
@@ -180,6 +304,7 @@ class CreateBandwidthsDialog(QDialog, Ui_bandwidths):
 
     def exit_procedure(self):
         self.close()
+
 
 if __name__ == '__main__':
     main()
