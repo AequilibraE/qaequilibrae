38fbeb0aea68e34bc46d65c7b92a5da0b513637b
veigacamargo@gmail.com <nUSP3126170>
2017-12-16 15:00:33 +1000

    Finishing "About" screen

--- common_tools/about_dialog.py
+++ common_tools/about_dialog.py
@@ -19,16 +19,16 @@
  -----------------------------------------------------------------------------------------------------------
  """
 
-from qgis.core import *
 from PyQt4 import QtGui, uic
 from PyQt4.QtGui import *
+import webbrowser
 
-import sys
 import os
 from auxiliary_functions import standard_path
 from ..aequilibrae.__version__ import release_name, release_version
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__),  'forms/ui_about.ui'))
+
 
 class AboutDialog(QtGui.QDialog, FORM_CLASS):
     def __init__(self, iface):
@@ -40,40 +40,41 @@ class AboutDialog(QtGui.QDialog, FORM_CLASS):
         self.but_mailing.clicked.connect(self.go_to_mailing_list)
         self.but_close.clicked.connect(self.exit_procedure)
 
-        # self.developers = ['Pedro Camargo', 'Jamie Cook (MacOS binaries)']
-        self.developers = 'Pedro Camargo, Jamie Cook (MacOS binaries)'
-        # self.sponsors = ['IPEA (2015)']
-        self.sponsors = 'IPEA (2015)'
+        repository = 'https://github.com/AequilibraE/AequilibraE'
+        self.wiki = "https://github.com/aequilibrae/aequilibrae/wiki"
+        sponsors = ['IPEA (2015)']
+        developers = ['Pedro Camargo', 'Jamie Cook (MacOS binaries)']
 
         self.all_items = []
         self.all_items.append(['Version name', release_name])
         self.all_items.append(['Version number', release_version])
+        self.all_items.append(['Repository', repository])
         self.all_items.append(['Minimum QGIS', '2.16'])
-        self.all_items.append(['Developers', self.developers])
-        self.all_items.append(['Sponsors', self.sponsors])
+        self.all_items.append(['Developers', developers])
+        self.all_items.append(['Sponsors', sponsors])
 
         self.assemble()
 
     def assemble(self):
         titles = []
-        currentRowCount = 0
+        row_count = 0
+
         for r, t in self.all_items:
             titles.append(r)
-            self.about_table.insertRow(currentRowCount)
+            self.about_table.insertRow(row_count)
+            if isinstance(t, list):
+                lv = QListWidget()
+                lv.addItems(t)
+                self.about_table.setCellWidget(row_count, 0, lv)
+                self.about_table.setRowHeight(row_count, len(t)*self.about_table.rowHeight(row_count))
+            else:
+                self.about_table.setItem(row_count, 0, QTableWidgetItem(str(t)))
 
-            # if isinstance(t, list):
-            #     tbl = QtGui.QTableWidget(len(t),1)
-            #     for i, item in enumerate(t):
-            #         tbl.setItem(i,0, QTableWidgetItem(item))
-            #     self.about_table.setCellWidget(currentRowCount, 0, tbl)
-            # else:
-            self.about_table.setItem(currentRowCount, 0, QTableWidgetItem(str(t)))
-            currentRowCount += 1
-
+            row_count += 1
         self.about_table.setVerticalHeaderLabels(titles)
+
     def go_to_mailing_list(self):
-
-
+        webbrowser.open(self.wiki, new=2)
         self.exit_procedure()
 
     def exit_procedure(self):
