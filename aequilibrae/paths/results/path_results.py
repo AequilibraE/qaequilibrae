027a3d9564f9c3e989f4715957e878e53bc29546
Pedro Camargo <veigacamargo@gmail.com>
2018-04-24 16:39:55 +1000

    Organizes new feature for recomputing trace on a pre-computed Path

--- aequilibrae/paths/results/path_results.py
+++ aequilibrae/paths/results/path_results.py
@@ -1,5 +1,7 @@
 import numpy as np
 from ..graph import Graph
+from ..AoN import update_path_trace
+
 
 class PathResults:
     def __init__(self):
@@ -50,39 +52,19 @@ class PathResults:
         else:
             print 'Exception: Path results object was not yet prepared/initialized'
 
+
     def update_trace(self, graph, destination):
         # type: (Graph, int) -> (None)
-
-        if self.__graph_id__ != graph.__id__:
-            raise ValueError("Results prepared for a different Graph")
-
         if not isinstance(destination, int):
-            raise TypeError('Destination needs to be an integer')
+            raise TypeError('destination needs to be an integer')
+
         if not isinstance(graph, Graph):
             raise TypeError('graph needs to be an AequilibraE Graph')
+
         if destination >= graph.nodes_to_indices.shape[0]:
             raise ValueError('destination out of the range of node numbers in the graph')
 
-        dest_index = graph.nodes_to_indices[destination]
-        origin_index = graph.nodes_to_indices[self.origin]
+        if self.__graph_id__ != graph.__id__:
+            raise ValueError("Results object not prepared. Use --> results.prepare(graph)")
 
-        self.destination = destination
-        self.milepost = None
-        self.path_nodes = None
-        if self.predecessors[dest_index] > 0:
-            all_connectors = []
-            all_nodes = [dest_index]
-            mileposts = []
-            p = dest_index
-            if p != origin_index:
-                while p != origin_index:
-                    p = self.predecessors[p]
-                    connector = self.connectors[dest_index]
-                    all_connectors.append(graph.graph['link_id'][connector])
-                    mileposts.append(graph.cost[connector])
-                    all_nodes.append(p)
-                    dest_index = p
-                self.path = np.asarray(all_connectors, graph.default_types('int'))[::-1]
-                self.path_nodes = graph.all_nodes[np.asarray(all_nodes, graph.default_types('int'))][::-1]
-                mileposts.append(0)
-                self.milepost = np.cumsum(mileposts[::-1])
+        update_path_trace(self, destination, graph)
