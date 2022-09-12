8e2a49dd5080f55b3b3d4250fe87ba9bb670e993
pveigadecamargo <pveigadecamargo@anl.gov>
2022-09-13 09:41:51 +1000

    Tests opening of Plugin

---
+++ tests/test_load_plugin.py
@@ -0,0 +1,28 @@
+import sqlite3
+import unittest
+import uuid
+from os.path import dirname, join
+
+from qgis.core import QgsCoordinateReferenceSystem
+from qgis.utils import iface
+from .utilities import get_qgis_app
+from ..AequilibraEMenu import AequilibraEMenu
+
+
+class OpenMainDialogTest(unittest.TestCase):
+    """Test dialog works."""
+
+    def setUp(self):
+        """Runs before each test."""
+        _ = get_qgis_app()
+
+    def test_if_dialog_opens(self):
+        """Test we can click OK."""
+
+        self.dialog = AequilibraEMenu(iface)
+
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(OpenMainDialogTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
