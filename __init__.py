f8bf96300b03d5b4b7fd604b2f70c92d848a5e69
pveigadecamargo <pveigadecamargo@anl.gov>
2022-09-13 10:11:27 +1000

    change path org

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "aequilibrae")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "aequilibrae")))
 
 
 def classFactory(iface):
