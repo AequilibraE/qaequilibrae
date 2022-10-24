9b50edd59b1700e824a5cbd1755c89f19e7060f3
TTalex <alex.bourreau@free.fr>
2022-10-24 18:13:50 +0200

    Avoid relative paths to make tests pass

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
 # import qgis libs so that ve set the correct sip api version
 
 import qgis  # pylint: disable=W0611  # NOQA
+import sys
+from os.path import abspath, join, dirname
+sys.path.insert(0, abspath(join(dirname(dirname(__file__)), "")))
+sys.path.insert(0, abspath(join(dirname(dirname(__file__)), "aequilibrae")))
