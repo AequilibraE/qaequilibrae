9104940aa4fd57574bd65eabc60f8e17c7e7ec00
TTalex <alex.bourreau@free.fr>
2023-01-20 11:55:03 +0100

    Added foundation for deeper gui tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,8 @@
 import pytest
-
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication
 from aequilibrae_menu import AequilibraEMenu
+from qaequilibrae.modules.common_tools import ReportDialog
 
 
 @pytest.fixture(scope="function")
@@ -16,3 +18,24 @@ def ae_with_project(qgis_iface) -> AequilibraEMenu:
     _run_load_project_from_path(ae, "test/data/SiouxFalls_project")
     yield ae
     ae.run_close_project()
+
+
+@pytest.fixture(scope="function")
+def timeoutDetector(qgis_iface) -> None:
+    def handle_trigger():
+        # Check if a report window has openned
+        window = QApplication.activeWindow()
+        if isinstance(window, ReportDialog):
+            window.close()
+            raise Exception("Test timed out because of a report dialog showing")
+        else:
+            if window:
+                window.close()
+            raise Exception("Test timed out")
+
+    timer = QTimer()
+    timer.timeout.connect(handle_trigger)
+    timer.setSingleShot(True)
+    timer.start(3000)
+    yield timer
+    timer.stop()
