a5e87f63ebdbbd22f399d411500d1cd72f6e6870
pveigadecamargo <pveigadecamargo@anl.gov>
2023-04-27 22:32:58 +1000

    prepares release

--- download_extra_packages_class.py
+++ download_extra_packages_class.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 from os.path import join
 from qgis.PyQt import uic, QtWidgets
+from qgis.core import QgsMessageLog, Qgis
 
 
 class download_all:
@@ -13,12 +14,13 @@ class download_all:
         self.pth = join(pth, "packages")
 
     def install(self):
-        spec = iutil.find_spec("aequilibrae")
-        if spec is not None:
+        sys.path.append(join(os.path.dirname(__file__), "packages"))
+        if iutil.find_spec("aequilibrae") is not None:
+            QgsMessageLog.logMessage("QAequilibraE are installed", level=Qgis.Critical)
             return
-
         lines = []
         command = f"python -m pip install -r {self.file} -t {self.pth}"
+        print(command)
         lines.append(command)
         with subprocess.Popen(
                 command,
@@ -29,3 +31,6 @@ class download_all:
                 universal_newlines=True,
         ) as proc:
             lines.extend(proc.stdout.readlines())
+
+        for line in lines:
+            QgsMessageLog.logMessage(str(line), level=Qgis.Critical)
