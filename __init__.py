76243721766bc493abfb17f44b7cbee0851788e0
Pedro Camargo <c@margo.co>
2023-05-06 17:03:15 +1000

    . (#211)
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>

--- __init__.py
+++ __init__.py
@@ -1,11 +1,4 @@
 # This portion of the script initializes the plugin, making it known to QGIS.
-import os
-import sys
-from os.path import abspath, join, dirname
-
-sys.path.append(join(os.path.dirname(__file__), "packages"))
-
-
 def classFactory(iface):
     from .aequilibrae_menu import AequilibraEMenu
     return AequilibraEMenu(iface)
