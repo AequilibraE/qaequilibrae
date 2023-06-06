60e77040508a947e57976001ef7f3b83dd7fda4f
Renata Imai <renataakemii@yahoo.co.uk>
2023-06-06 08:37:30 -0300

    updates test

--- test/test_pt_procedures.py
+++ test/test_pt_procedures.py
@@ -7,12 +7,26 @@ from qaequilibrae.modules.public_transport_procedures.gtfs_feed import GTFSFeed
 from qaequilibrae.modules.public_transport_procedures.gtfs_importer import GTFSImporter
 
 
-def test_click(ae_with_project, qtbot):
+def test_click_importer(ae_with_project, qtbot):
     dialog = GTFSImporter(ae_with_project)
     dialog.show()
 
     assert dialog.label_3.text() == "Add Transit Table"
     assert dialog.rdo_clear.text() == "Crete New Route System"
+
+    with qtbot.capture_exceptions() as exceptions:
+        qtbot.mouseClick(dialog.but_execute, Qt.LeftButton)
+
+    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
+
+
+def test_click_feed(transit_object, pt_project, qtbot):
+    dialog = GTFSFeed(transit_object, pt_project)
+    dialog.show()
+
+    assert dialog.label_2.text() == "Service date"
+    assert dialog.label_3.text() == "Agency*"
+    assert dialog.label_4.text() == "Description*"
 
     with qtbot.capture_exceptions() as exceptions:
         qtbot.mouseClick(dialog.but_add, Qt.LeftButton)
