fd9a5cb16ed8ee743b4ff00c23c09c494459f49a
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2023-06-15 05:05:30 -0300

    Gtfs importer (#218)
    * gtfs_menu
    
    * gtfs_menu
    
    * gtfs_menu
    
    * change functions
    
    * improves workflow
    
    * update functions
    
    * changes ui
    
    * updates gtfs_importer.py
    
    * Changes text
    
    * changes imports
    
    * black
    
    * fixes function names in menu
    
    * fixes upper case
    
    * adds pt procedures test
    
    * adds test
    
    * fixes pt test
    
    * updates test
    
    * Changes in pt procedure tests
    
    * Changes in pt procedure tests
    
    * Updates test/test_pt_procedures.py
    
    * Adds file
    
    * Changes pt procedure test
    
    * Changes pt procedure test
    
    * Changes pt procedure test
    
    * Changes pt procedure test
    
    * Changes pt procedure test
    
    * Changes pt procedure test
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Modifies PT Procedures test
    
    * Modifies PT Procedures test
    
    * Modifies PT Procedures test
    
    * Modifies PT Procedures test
    
    * Modifies PT Procedures test
    
    * Modifies PT Procedures test
    
    ---------
    
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>

---
+++ test/test_pt_procedures.py
@@ -0,0 +1,73 @@
+import os
+import pytest
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication
+from qgis.core import QgsProject, Qgis, QgsVectorLayer
+from qaequilibrae.modules.public_transport_procedures.gtfs_feed import GTFSFeed
+from qaequilibrae.modules.public_transport_procedures.gtfs_importer import GTFSImporter
+
+
+def test_click_new_importer(ae_with_project, qtbot):
+    dialog = GTFSImporter(ae_with_project)
+    dialog.show()
+
+    assert dialog.label_3.text() == "Add Transit Table"
+    assert dialog.rdo_clear.text() == "Crete New Route System"
+
+    with qtbot.capture_exceptions() as exceptions:
+        qtbot.mouseClick(dialog.but_execute, Qt.LeftButton)
+
+    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
+    
+    dialog.close()
+
+
+def test_click_add_importer(pt_project, qtbot):
+    dialog = GTFSImporter(pt_project)
+    dialog.show()
+
+    assert dialog.rdo_clear.text() == "Overwrite Routes"
+    assert dialog.rdo_keep.text() == "Add to Existing Routes"
+
+    with qtbot.capture_exceptions() as exceptions:
+        qtbot.mouseClick(dialog.but_execute, Qt.LeftButton)
+
+    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
+
+    dialog.close()
+
+
+def test_click_feed(pt_project, qtbot):
+    from aequilibrae.transit import Transit
+    
+    data = Transit(pt_project.project)
+
+    dialog = GTFSFeed(pt_project, data, True)
+    dialog.show()
+    qtbot.addWidget(dialog)
+    qtbot.waitExposed(dialog)
+
+    assert dialog.label.text() == "Route capacities"
+    assert dialog.label_2.text() == "Service date"
+    assert dialog.label_3.text() == "Description*"
+    assert dialog.label_4.text() == "Agency*"
+
+    with qtbot.capture_exceptions() as exceptions:
+        qtbot.mouseClick(dialog.but_new_row, Qt.LeftButton)
+
+    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
+
+
+def test_pt_menu(ae_with_project, qtbot):
+    from qaequilibrae.modules.public_transport_procedures.gtfs_importer import GTFSImporter
+    from test.test_qaequilibrae_menu_with_project import check_if_new_active_window_matches_class
+
+    def handle_trigger():
+        check_if_new_active_window_matches_class(qtbot, GTFSImporter)
+
+    action = ae_with_project.menuActions["Public Transport"][0]
+    assert action.text() == "Import GTFS", "Wrong text content"
+    QTimer.singleShot(10, handle_trigger)
+    action.trigger()
+    messagebar = ae_with_project.iface.messageBar()
+    assert len(messagebar.messages[3]) == 0, "Messagebar should be empty" + str(messagebar.messages)
