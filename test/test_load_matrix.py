208be24df0a67cbac293f81793c1c1e35b5dad09
Renata Imai <renataakemii@yahoo.co.uk>
2023-06-19 13:57:45 -0300

    Adds test

--- test/test_load_matrix.py
+++ test/test_load_matrix.py
@@ -19,3 +19,16 @@ def test_mat_menu(ae_with_project, qtbot):
     action.trigger()
     messagebar = ae_with_project.iface.messageBar()
     assert len(messagebar.messages[3]) == 0, "Messagebar should be empty" + str(messagebar.messages)
+
+
+def test_mat_load(ae_with_project, qtbot):
+    dialog = LoadMatrixDialog(ae_with_project)
+    dialog.show()
+
+    assert dialog.radio_layer_matrix.text() == "Open layer"
+
+
+    qtbot.mouseClick(dialog.radio_layer_matrix, Qt.LeftButton)
+    qtbot.mouseClick(dialog.but_load, Qt.LeftButton)
+    # qtbot.mouseClick(dialog.but_permanent_save, Qt.LeftButton)
+    qtbot.stop()
