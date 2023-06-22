304c478b6435084715d4811f055725f6dcdc80f1
Pedro Camargo <c@margo.co>
2023-06-22 03:46:45 -0700

    Typo (#224)
    * Typo
    
    * removes excessive capitalization
    
    * Changes PT Procedures test
    
    * Changes assertion in test
    
    * Changes assertion in test
    
    * Changes assertion in test
    
    * Changes assertion in test[B
    
    * Check if feed is not None
    
    * Check if feed info is added
    
    * Updates PT procedure test
    
    * Updates PT procedure test
    
    * Updates PT procedure test
    
    * Updates PT procedure test
    
    * Changes PT Procedures Test
    
    * Removes test skip
    
    * Changes PT Procedures test
    
    * Changes PT Procedures test
    
    * Changes in GTFS Feed
    
    * Modifies test
    
    * Changes PT Proceduresd test
    
    * Adds file and changes test
    
    * Temporarily changes requirements
    
    * Modifies PT Procedure Tests
    
    ---------
    
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>
    Co-authored-by: Renata Imai <renataakemii@yahoo.co.uk>
    Co-authored-by: Renata Imai <53949163+r-akemii@users.noreply.github.com>

--- test/test_pt_procedures.py
+++ test/test_pt_procedures.py
@@ -1,61 +1,9 @@
 import os
+import sqlite3
 import pytest
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QApplication
-from qgis.core import QgsProject, Qgis, QgsVectorLayer
+from PyQt5.QtCore import QTimer
 from qaequilibrae.modules.public_transport_procedures.gtfs_feed import GTFSFeed
 from qaequilibrae.modules.public_transport_procedures.gtfs_importer import GTFSImporter
-
-
-def test_click_new_importer(ae_with_project, qtbot):
-    dialog = GTFSImporter(ae_with_project)
-    dialog.show()
-
-    assert dialog.label_3.text() == "Add Transit Table"
-    assert dialog.rdo_clear.text() == "Crete New Route System"
-
-    with qtbot.capture_exceptions() as exceptions:
-        qtbot.mouseClick(dialog.but_execute, Qt.LeftButton)
-
-    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
-    
-    dialog.close()
-
-
-def test_click_add_importer(pt_project, qtbot):
-    dialog = GTFSImporter(pt_project)
-    dialog.show()
-
-    assert dialog.rdo_clear.text() == "Overwrite Routes"
-    assert dialog.rdo_keep.text() == "Add to Existing Routes"
-
-    with qtbot.capture_exceptions() as exceptions:
-        qtbot.mouseClick(dialog.but_execute, Qt.LeftButton)
-
-    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
-
-    dialog.close()
-
-
-def test_click_feed(pt_project, qtbot):
-    from aequilibrae.transit import Transit
-    
-    data = Transit(pt_project.project)
-
-    dialog = GTFSFeed(pt_project, data, True)
-    dialog.show()
-    qtbot.addWidget(dialog)
-    qtbot.waitExposed(dialog)
-
-    assert dialog.label.text() == "Route capacities"
-    assert dialog.label_2.text() == "Service date"
-    assert dialog.label_3.text() == "Description*"
-    assert dialog.label_4.text() == "Agency*"
-
-    with qtbot.capture_exceptions() as exceptions:
-        qtbot.mouseClick(dialog.but_new_row, Qt.LeftButton)
-
-    assert len(exceptions) == 0, "Exception shouldn't be raised all the way to here"
 
 
 def test_pt_menu(ae_with_project, qtbot):
@@ -71,3 +19,79 @@ def test_pt_menu(ae_with_project, qtbot):
     action.trigger()
     messagebar = ae_with_project.iface.messageBar()
     assert len(messagebar.messages[3]) == 0, "Messagebar should be empty" + str(messagebar.messages)
+
+
+def test_add_new_feed(pt_no_feed):
+    from aequilibrae.transit import Transit
+
+    importer = GTFSImporter(pt_no_feed)
+    assert importer.label_3.text() == "Add transit table"
+    assert importer.rdo_clear.text() == "Create new route system"
+
+    data = Transit(pt_no_feed.project)
+    feed = GTFSFeed(pt_no_feed, data, True)
+
+    assert feed.label.text() == "Route capacities"
+    assert feed.label_2.text() == "Service date"
+    assert feed.label_3.text() == "Description*"
+    assert feed.label_4.text() == "Agency*"
+
+    gtfs_file = "test/data/coquimbo_project/gtfs_coquimbo.zip"
+    feed.set_data(gtfs_file)
+    feed.led_agency.setText("New agency")
+    feed.led_description.setText("Adds new agency description")
+    feed.date = "2016-04-13"
+    feed.return_feed()
+
+    importer.set_feed(feed.feed)
+    importer.rdo_keep.setChecked(True)
+    importer.execute_importer()
+
+    db_path = os.path.join(pt_no_feed.project.project_base_path, "public_transport.sqlite")
+    conn = sqlite3.connect(db_path)
+    var = conn.execute("select count(agency_id) from agencies").fetchone()[0]
+
+    assert var == 1
+
+
+@pytest.mark.parametrize(
+    ("is_checked", "set_date", "set_agency"),
+    [(False, "2016-06-17", "New agency"), (True, "2016-08-21", "Other agency")],
+)
+def test_add_other_feed(pt_project, set_agency, set_date, is_checked):
+    from aequilibrae.transit import Transit
+
+    importer = GTFSImporter(pt_project)
+    assert importer.label_3.text() == "Resetting Transit Tables"
+    assert importer.rdo_clear.text() == "Overwrite Routes"
+    assert importer.rdo_keep.text() == "Add to Existing Routes"
+
+    data = Transit(pt_project.project)
+    feed = GTFSFeed(pt_project, data, True)
+
+    assert feed.label.text() == "Route capacities"
+    assert feed.label_2.text() == "Service date"
+    assert feed.label_3.text() == "Description*"
+    assert feed.label_4.text() == "Agency*"
+
+    gtfs_file = "test/data/coquimbo_project/gtfs_coquimbo.zip"
+    feed.set_data(gtfs_file)
+    feed.led_agency.setText(set_agency)
+    feed.led_description.setText("Adds new agency description")
+    feed.date = set_date
+    feed.return_feed()
+
+    importer.rdo_clear.setChecked(is_checked)
+    if not is_checked:
+        importer.rdo_keep.setChecked(True)
+    importer.set_feed(feed.feed)
+    importer.execute_importer()
+
+    db_path = os.path.join(pt_project.project.project_base_path, "public_transport.sqlite")
+    conn = sqlite3.connect(db_path)
+    var = conn.execute("select count(agency_id) from agencies").fetchone()[0]
+
+    if is_checked:
+        assert var == 1
+    else:
+        assert var == 2
