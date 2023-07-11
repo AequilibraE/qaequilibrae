58deef29171c0326936e5232d6dac3e0b824e3df
pveigadecamargo <pveigadecamargo@anl.gov>
2023-07-12 08:02:02 +1000

    fixes imports

--- test/test_gis_desire_lines.py
+++ test/test_gis_desire_lines.py
@@ -1,8 +1,10 @@
 import os
+
 import pytest
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QApplication
 from qgis.core import QgsProject, Qgis, QgsVectorLayer, QgsCoordinateReferenceSystem
+
 from qaequilibrae.modules.gis.desire_lines_dialog import DesireLinesDialog
 
 
