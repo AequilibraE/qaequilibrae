c8a8f5ba5e604160961f0130a7658617e16fbc14
Renata Imai <renataakemii@yahoo.co.uk>
2023-07-17 10:55:08 -0300

    Updates files for test_traffic_assignment.py

--- test/test_traffic_assignment.py
+++ test/test_traffic_assignment.py
@@ -28,7 +28,7 @@ def test_single_class_traffic_assignment(ae_with_project, qtbot):
 
     dialog = TrafficAssignmentDialog(ae_with_project)
 
-    test_name = f"TestTrafficAssignment_{uuid4().hex[:6]}"
+    test_name = f"TestTrafficAssignment_SC_{uuid4().hex[:6]}"
     dialog.cob_matrices.setCurrentText("b'none'")
     dialog.ln_class_name.setText("car")
     dialog.cob_mode_for_class.setCurrentText("car (c)")
@@ -38,12 +38,10 @@ def test_single_class_traffic_assignment(ae_with_project, qtbot):
     dialog.cob_skim_class.setCurrentText("car")
     dialog.chb_check_centroids.setChecked(False)
     dialog.output_scenario_name.setText(test_name)
-
     qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
 
     dialog.cob_skims_available.setCurrentText("free_flow_time")
     qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
-
     dialog.cob_skims_available.setCurrentText("distance")
     qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
 
@@ -53,7 +51,7 @@ def test_single_class_traffic_assignment(ae_with_project, qtbot):
     dialog.cob_capacity.setCurrentText("capacity")
     dialog.cob_ffttime.setCurrentText("free_flow_time")
     dialog.cb_choose_algorithm.setCurrentText("bfw")
-    dialog.max_iter.setText("30")
+    dialog.max_iter.setText("500")
     dialog.rel_gap.setText("0.001")
 
     dialog.run()
@@ -61,17 +59,18 @@ def test_single_class_traffic_assignment(ae_with_project, qtbot):
     pth = Path("test/data/SiouxFalls_project")
     results = pth / "results_database.sqlite"
     assert isfile(results)
+
     con = sqlite3.connect(results)
-    assert con.execute(f"SELECT SUM(PCE_tot) FROM {test_name}").fetchone()[0] > 881_000
+    assert con.execute(f"SELECT ROUND(SUM(PCE_tot), 4) FROM {test_name}").fetchone()[0] == 879087.8481
 
     skims = pth / ("matrices/" + test_name + "_car.omx")
     assert isfile(skims)
 
     mtx = omx.open_file(skims)
-    assert np.sum(np.nan_to_num(mtx["free_flow_time_final"][:])) > 13_500
-    assert np.sum(np.nan_to_num(mtx["free_flow_time_blended"][:])) > 13_500
-    assert np.sum(np.nan_to_num(mtx["distance_final"][:])) > 6_500
-    assert np.sum(np.nan_to_num(mtx["distance_blended"][:])) > 6_700
+    assert round(np.sum(np.nan_to_num(mtx["free_flow_time_final"][:])), 4) == 13594.6657
+    assert round(np.sum(np.nan_to_num(mtx["free_flow_time_blended"][:])), 4) == 13548.7296
+    assert round(np.sum(np.nan_to_num(mtx["distance_final"][:])), 4) == 6584.0
+    assert round(np.sum(np.nan_to_num(mtx["distance_blended"][:])), 4) == 6719.0863
 
     num_cores = dialog.assignment.cores
     log_ = pth / "aequilibrae.log"
@@ -88,7 +87,6 @@ def test_single_class_traffic_assignment(ae_with_project, qtbot):
     assert "INFO ; Traffic Assignment specification" in file_text
     assert "{{'VDF parameters': {{'alpha': 0.15, 'beta': 4.0}}, 'VDF function': 'bpr', 'Number of cores': {}, 'Capacity field': 'capacity', 'Time field': 'free_flow_time', 'Algorithm': 'bfw', 'Maximum iterations': 30, 'Target RGAP': 0.001}}".format(num_cores)
 
-@pytest.mark.skip(reason="It's not working yet")
 def test_multiclass_traffic_assignment(ae_with_project, qtbot):
     update_matrices = LoadProjectDataDialog(ae_with_project)
     update_matrices.update_matrix_table()
@@ -107,12 +105,13 @@ def test_multiclass_traffic_assignment(ae_with_project, qtbot):
     dialog.pce_setter.setValue(1.0)
     qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
 
-    dialog.ln_class_name.setText("motorcycle")
-    dialog.cob_mode_for_class.setCurrentIndex(3)
-    dialog.tbl_core_list.selectRow(1)
-    dialog.chb_check_centroids.setChecked(False)
-    dialog.pce_setter.setValue(0.2)
-    qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
+    # Adds car skims
+    dialog.cob_skims_available.setCurrentText("free_flow_time")
+    dialog.cob_skim_class.setCurrentText("car")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+    dialog.cob_skims_available.setCurrentText("distance")
+    dialog.cob_skim_class.setCurrentText("car")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
 
     dialog.ln_class_name.setText("truck")
     dialog.cob_mode_for_class.setCurrentText("trucks (T)")
@@ -122,25 +121,60 @@ def test_multiclass_traffic_assignment(ae_with_project, qtbot):
     dialog.pce_setter.setValue(2.5)
     qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
 
+    # Adds truck skims
+    dialog.cob_skims_available.setCurrentText("free_flow_time")
+    dialog.cob_skim_class.setCurrentText("truck")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+    dialog.cob_skims_available.setCurrentText("distance")
+    dialog.cob_skim_class.setCurrentText("truck")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+
+    dialog.ln_class_name.setText("motorcycle")
+    dialog.cob_mode_for_class.setCurrentIndex(3)
+    dialog.tbl_core_list.selectRow(1)
+    dialog.chb_check_centroids.setChecked(False)
+    dialog.pce_setter.setValue(0.2)
+    qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
+
+    # Adds motorcycle skims
+    dialog.cob_skims_available.setCurrentText("free_flow_time")
+    dialog.cob_skim_class.setCurrentText("motorcycle")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+    dialog.cob_skims_available.setCurrentText("distance")
+    dialog.cob_skim_class.setCurrentText("motorcycle")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+
+    dialog.skims["car"] = ["free_flow_time", "distance"]
+    dialog.skims["Trucks"] = ["free_flow_time", "distance"]
+    dialog.skims["Motorcycle"] = ["free_flow_time", "distance"]
+
+    # Assignment setup
     dialog.tbl_vdf_parameters.cellWidget(0, 1).setText("0.15")
     dialog.tbl_vdf_parameters.cellWidget(1, 1).setText("4.0")
     dialog.cob_vdf.setCurrentText("BPR")
     dialog.cob_capacity.setCurrentText("capacity")
     dialog.cob_ffttime.setCurrentText("free_flow_time")
     dialog.cb_choose_algorithm.setCurrentText("bfw")
-    dialog.max_iter.setText("30")
+    dialog.max_iter.setText("20")
     dialog.rel_gap.setText("0.001")
 
     dialog.run()
+
+    with pytest.raises(ValueError):
+        dialog.produce_all_outputs()
 
     pth = Path("test/data/SiouxFalls_project")
     results = pth / "results_database.sqlite"
     assert isfile(results)
     con = sqlite3.connect(results)
-    assert con.execute(f"SELECT SUM(PCE_tot) FROM {test_name}").fetchone()[0] > 152_000
-    assert con.execute(f"SELECT SUM(car_tot) FROM {test_name}").fetchone()[0] > 81_000
-    assert con.execute(f"SELECT SUM(motorcycle_tot) FROM {test_name}").fetchone()[0] > 27_000
-    assert con.execute(f"SELECT SUM(trucks_tot) FROM {test_name}").fetchone()[0] > 26_000
+    assert con.execute(f"SELECT ROUND(SUM(PCE_tot), 4) FROM {test_name}").fetchone()[0] == 1332054.7483
+    assert con.execute(f"SELECT ROUND(SUM(car_tot), 4) FROM {test_name}").fetchone()[0] == 698325.9227
+    assert con.execute(f"SELECT ROUND(SUM(motorcycle_tot), 4) FROM {test_name}").fetchone()[0] == 242112.6235
+    assert con.execute(f"SELECT ROUND(SUM(trucks_tot), 4) FROM {test_name}").fetchone()[0] == 234122.5204
+
+    assert isfile(pth / ("matrices/" + test_name + "_car.omx"))
+    assert isfile(pth / ("matrices/" + test_name + "_motorcycle.omx"))
+    assert isfile(pth / ("matrices/" + test_name + "_trucks.omx"))
 
     num_cores = dialog.assignment.cores
     log_ = pth / "aequilibrae.log"
@@ -153,8 +187,10 @@ def test_multiclass_traffic_assignment(ae_with_project, qtbot):
 
     assert "INFO ; Traffic Class specification" in file_text
     assert """INFO ; {'car': {'Graph': "{'Mode': 'c', 'Block through centroids': False, 'Number of centroids': 24, 'Links': 76, 'Nodes': 24}",""" in file_text
-    assert """'Number of centroids': 24, 'Matrix cores': ['matrix'], 'Matrix totals': {'matrix': 360600.0}}"}}""" in file_text
-    # Check for motorcycle and truck
-
+    assert """'Number of centroids': 24, 'Matrix cores': ['car'], 'Matrix totals': {'car': 271266.6324170904}}"}}""" in file_text
+    assert """INFO ; {'motorcycles': {'Graph': "{'Mode': 'M', 'Block through centroids': False, 'Number of centroids': 24, 'Links': 76, 'Nodes': 24}","""
+    assert """'Number of centroids': 24, 'Matrix cores': ['motorcycle'], 'Matrix totals': {'motorcycle': 89819.0708124364}}"}}"""
+    assert """INFO ; {'trucks': {'Graph': "{'Mode': 'T', 'Block through centroids': False, 'Number of centroids': 24, 'Links': 76, 'Nodes': 24}","""
+    assert """'Number of centroids': 24, 'Matrix cores': ['trucks'], 'Matrix totals': {'trucks': 90235.57459796841}}"}}"""
     assert "INFO ; Traffic Assignment specification" in file_text
-    assert "{{'VDF parameters': {{'alpha': 0.15, 'beta': 4.0}}, 'VDF function': 'bpr', 'Number of cores': {}, 'Capacity field': 'capacity', 'Time field': 'free_flow_time', 'Algorithm': 'bfw', 'Maximum iterations': 30, 'Target RGAP': 0.001}}".format(num_cores)
+    assert "{{'VDF parameters': {{'alpha': 0.15, 'beta': 4.0}}, 'VDF function': 'bpr', 'Number of cores': {}, 'Capacity field': 'capacity', 'Time field': 'free_flow_time', 'Algorithm': 'bfw', 'Maximum iterations': 20, 'Target RGAP': 0.001}}".format(num_cores)
