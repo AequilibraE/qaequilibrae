c366b3df3bff1feb5f94fadd4bed6cf05871ce2f
Renata Imai <renataakemii@yahoo.co.uk>
2023-10-12 17:00:41 -0300

    add test

---
+++ test/test_distribution_procedures.py
@@ -0,0 +1,81 @@
+import sqlite3
+# from os.path import isfile
+from pathlib import Path
+# from uuid import uuid4
+
+import numpy as np
+import openmatrix as omx
+import pytest
+from PyQt5.QtCore import QTimer, Qt
+
+from qaequilibrae.modules.distribution_procedures.distribution_models_dialog import DistributionModelsDialog
+from qaequilibrae.modules.paths_procedures.traffic_assignment_dialog import TrafficAssignmentDialog
+
+
+def traffic_assignment(ae_with_project, qtbot):    
+    dialog = TrafficAssignmentDialog(ae_with_project)
+
+    assignment_result = f"TrafficAssignment_DP"
+    dialog.output_scenario_name.setText(assignment_result)
+    dialog.cob_matrices.setCurrentText("demand.aem")
+
+    dialog.tbl_core_list.selectRow(0)
+    dialog.cob_mode_for_class.setCurrentIndex(0)
+    dialog.ln_class_name.setText("car")
+    dialog.pce_setter.setValue(1.0)
+    dialog.chb_check_centroids.setChecked(False)
+    qtbot.mouseClick(dialog.but_add_class, Qt.LeftButton)
+
+    dialog.cob_skims_available.setCurrentText("free_flow_time")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+    dialog.cob_skims_available.setCurrentText("distance")
+    qtbot.mouseClick(dialog.but_add_skim, Qt.LeftButton)
+
+    dialog.tbl_vdf_parameters.cellWidget(0, 1).setText("0.15")
+    dialog.tbl_vdf_parameters.cellWidget(1, 1).setText("4.0")
+    dialog.cob_vdf.setCurrentText("BPR")
+    dialog.cob_capacity.setCurrentText("capacity")
+    dialog.cob_ffttime.setCurrentText("free_flow_time")
+    dialog.cb_choose_algorithm.setCurrentText("bfw")
+    dialog.max_iter.setText("500")
+    dialog.rel_gap.setText("0.001")
+
+    dialog.run()
+    
+
+def test_iterative_proportional_fitting(ae_with_project, qtbot):
+    dialog = DistributionModelsDialog(ae_with_project, mode="ipf")
+
+    pass
+
+def test_calibrate_gravity(ae_with_project, qtbot):
+    # traffic_assignment(ae_with_project, qtbot)
+
+    dialog = DistributionModelsDialog(ae_with_project, mode="calibrate", testing=True)
+
+    dialog.path = "test/data/SiouxFalls_project/"
+    dialog.out_name = "test/data/SiouxFalls_project/mod_negative_exponential.mod"
+    dialog.cob_imped_mat.setCurrentIndex(5)
+    dialog.cob_imped_field.setCurrentText("free_flow_time_final")
+    dialog.cob_seed_mat.setCurrentIndex(4)
+    dialog.cob_seed_field.setCurrentText("matrix")
+    dialog.rdo_expo.setChecked(True)
+
+    qtbot.mouseClick(dialog.but_queue, Qt.LeftButton)
+    qtbot.mouseClick(dialog.but_run, Qt.LeftButton)
+
+    dialog.close()
+
+    # Check if mod file was saved and contains any information
+
+def test_apply_gravity(ae_with_project, qtbot):
+    dialog = DistributionModelsDialog(ae_with_project, mode="apply", testing=True)
+
+    dialog.path = "test/data/SiouxFalls_project/"
+    dialog.cob_imped_mat.setCurrentIndex(5)
+    dialog.cob_imped_field.setCurrentText("free_flow_time_final")
+
+    qtbot.mouseClick(dialog.but_queue, Qt.LeftButton)
+    qtbot.mouseClick(dialog.but_run, Qt.LeftButton)
+
+    dialog.close()
