b5e7bb6a6e8f317af9e3b1fc2c29a1fcff72aa8e
Renata Imai <renataakemii@yahoo.co.uk>
2023-12-06 18:48:30 -0300

    Update test_adds_connector.py

--- test/test_adds_connector.py
+++ test/test_adds_connector.py
@@ -9,6 +9,11 @@ from qgis.PyQt.QtCore import QVariant
 from qaequilibrae.modules.network.adds_connectors_dialog import AddConnectorsDialog
 
 
+def commit_deletion(connection):
+    connection.execute("delete from links where name like 'centroid%'")
+    connection.execute("delete from nodes where is_centroid=1")
+    connection.commit()
+
 def test_add_connectors_from_zones(pt_project):
     dialog = AddConnectorsDialog(pt_project)
     dialog.rdo_zone.setChecked(True)
@@ -30,16 +35,17 @@ def test_add_connectors_from_zones(pt_project):
     assert node_count == 11
     assert link_count == 11
 
-    conn.execute("delete from links where name like 'centroid%'")
-    conn.execute("delete from nodes where is_centroid=1")
-    conn.commit()
+    # conn.execute("delete from links where name like 'centroid%'")
+    # conn.execute("delete from nodes where is_centroid=1")
+    # conn.commit()
 
+    commit_deletion(conn)
 
 @pytest.mark.parametrize(
-    ("node_id", "radius", "point"), [(1000, 500, Point(-96.90, 43.4907)), (1001, 5555, Point(-96.90, 43.4910))]
+    ("node_id", "radius", "point"), [(1000, 500, Point(-29.9170, -71.3183)), (1001, 5555, Point(-29.9176, -71.3657))]
 )
-def test_add_connectors_from_network(ae_with_project, node_id, radius, point):
-    dialog = AddConnectorsDialog(ae_with_project)
+def test_add_connectors_from_network(pt_project, node_id, radius, point):
+    dialog = AddConnectorsDialog(pt_project)
     dialog.rdo_network.setChecked(True)
 
     # Add a node to the network
@@ -57,17 +63,19 @@ def test_add_connectors_from_network(ae_with_project, node_id, radius, point):
 
     assert dialog.sb_radius.value() == radius
 
-    db_path = os.path.join(ae_with_project.project.project_base_path, "project_database.sqlite")
+    db_path = os.path.join(pt_project.project.project_base_path, "project_database.sqlite")
     conn = sqlite3.connect(db_path)
-    node_count = conn.execute("select count(node_id) from nodes where modes is null").fetchone()[0]
-    link_count = conn.execute("select count(name) from links where name is not null").fetchone()[0]
+    node_count = conn.execute("select count(node_id) from nodes where is_centroid=1").fetchone()[0]
+    link_count = conn.execute("select count(name) from links where name like 'centroid connector%'").fetchone()[0]
 
     if radius == 500:
         assert node_count == 1
         assert link_count == 0
+        commit_deletion(conn)
     else:
-        assert node_count == 2
-        assert link_count == 2
+        assert node_count == 1
+        assert link_count == 1
+        commit_deletion(conn)
 
 @pytest.mark.skip("Test is crashing")
 def test_add_connectors_from_layer(ae_with_project):
