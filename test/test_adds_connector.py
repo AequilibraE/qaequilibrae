dc81c463fefb830550a3db4e9dc21c489aae3c13
Renata Imai <renataakemii@yahoo.co.uk>
2023-12-06 16:05:04 -0300

    updates test

--- test/test_adds_connector.py
+++ test/test_adds_connector.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import pytest
+from shapely.geometry import Point
 from qgis.core import QgsProject, QgsVectorLayer, QgsField, QgsFeature
 from qgis.core import QgsFeature, QgsPointXY, QgsGeometry
 from qgis.PyQt.QtCore import QVariant
@@ -9,7 +10,6 @@ from qaequilibrae.modules.network.adds_connectors_dialog import AddConnectorsDia
 
 
 def load_layer():
-
     nodes_layer = QgsVectorLayer("Point?crs=epsg:4326", "Nodes layer", "memory")
     if not nodes_layer.isValid():
         print("Nodes layer failed to load!")
@@ -41,37 +41,65 @@ def load_layer():
         QgsProject.instance().addMapLayer(nodes_layer)
 
 
-@pytest.mark.skip("not working yet")  
-def test_add_connectors_from_zones(ae_with_project, qtbot):
-    dialog = AddConnectorsDialog(ae_with_project)
+def test_add_connectors_from_zones(pt_project):
+    dialog = AddConnectorsDialog(pt_project)
     dialog.rdo_zone.setChecked(True)
 
     # Checks if there is a zone system in the project
     zoning = dialog.project.zoning
     assert len(zoning.all_zones())
 
-    # dialog.lst_link_types.setCurrentRow(0)
-    # dialog.lst_modes.setCurrentRow(3)
+    dialog.lst_modes.setCurrentRow(1)
+    dialog.lst_link_types.setCurrentRow(11)
 
-    # dialog.run()
+    dialog.run()
 
-    # Assert in the links and nodes tables that the connector was created.
+    db_path = os.path.join(pt_project.project.project_base_path, "project_database.sqlite")
+    conn = sqlite3.connect(db_path)
+    node_count = conn.execute("select count(node_id) from nodes where is_centroid=1").fetchone()[0]
+    link_count = conn.execute("select count(name) from links where name like 'centroid connector%'").fetchone()[0]
 
-@pytest.mark.skip("not working yet")
-def test_add_connectors_from_network(ae_with_project):
+    assert node_count == 11
+    assert link_count == 11
+
+
+@pytest.mark.parametrize(
+    ("node_id", "radius", "point"), [(1000, 500, Point(-96.90, 43.4907)), (1001, 5555, Point(-96.90, 43.4910))]
+)
+def test_add_connectors_from_network(ae_with_project, node_id, radius, point):
     dialog = AddConnectorsDialog(ae_with_project)
     dialog.rdo_network.setChecked(True)
 
-    # dialog.lst_link_types.setCurrentRow(0)
-    # dialog.lst_modes.setCurrentRow(3)
+    # Add a node to the network
+    nodes = dialog.project.network.nodes
+    nd = nodes.new_centroid(node_id)
+    nd.geometry = point
+    nd.save()
 
-    # dialog.run()
+    dialog.lst_link_types.setCurrentRow(1)
+    dialog.lst_modes.setCurrentRow(3)
 
-    # Assert in the links and nodes tables that the connector was created.
+    dialog.sb_radius.setValue(radius)
+
+    dialog.run()
+
+    assert dialog.sb_radius.value() == radius
+
+    db_path = os.path.join(ae_with_project.project.project_base_path, "project_database.sqlite")
+    conn = sqlite3.connect(db_path)
+    node_count = conn.execute("select count(node_id) from nodes where modes is null").fetchone()[0]
+    link_count = conn.execute("select count(name) from links where name is not null").fetchone()[0]
+
+    if radius == 500:
+        assert node_count == 1
+        assert link_count == 0
+    else:
+        assert node_count == 2
+        assert link_count == 2
 
 
 def test_add_connectors_from_layer(ae_with_project):
-    load_layer() # load points layer
+    load_layer()  # load points layer
 
     dialog = AddConnectorsDialog(ae_with_project)
     dialog.rdo_layer.setChecked(True)
@@ -83,8 +111,9 @@ def test_add_connectors_from_layer(ae_with_project):
 
     db_path = os.path.join(ae_with_project.project.project_base_path, "project_database.sqlite")
     conn = sqlite3.connect(db_path)
-    var = conn.execute("select count(name) from links where name is not null").fetchone()[0]
-    assert var == 2
-
     var = conn.execute("select count(node_id) from nodes where modes is null").fetchone()[0]
-    assert var == 3
+    assert var == 5
+
+    # Due to the radius, not all nodes are connected to the network
+    var = conn.execute("select count(name) from links where name is not null").fetchone()[0]
+    assert var == 2  # check the validity of this assertion!
