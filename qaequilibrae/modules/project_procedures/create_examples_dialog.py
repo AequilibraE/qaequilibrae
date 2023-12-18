0aff48b85272551b92257b43a037a59437606055
Renata Imai <renataakemii@yahoo.co.uk>
2023-12-18 15:45:45 -0300

    .

--- qaequilibrae/modules/project_procedures/create_examples_dialog.py
+++ qaequilibrae/modules/project_procedures/create_examples_dialog.py
@@ -1,21 +1,19 @@
-import logging
 import os
 from os.path import isdir, join
 
 from PyQt5.QtCore import Qt
-from aequilibrae.utis.create_example import create_example
+from aequilibrae.utils.create_example import create_example
 
-from qaequilibrae.modules.common_tools import reporter
 from qgis.PyQt import QtWidgets, uic
-from qgis.PyQt.QtWidgets import QProgressBar, QLabel, QVBoxLayout, QGroupBox
+from qgis.PyQt.QtWidgets import QVBoxLayout, QGroupBox
 from qgis.PyQt.QtWidgets import QRadioButton, QGridLayout, QPushButton, QLineEdit
 from qgis.PyQt.QtWidgets import QWidget, QFileDialog
-from qaequilibrae.modules.common_tools import ReportDialog, standard_path
+from qaequilibrae.modules.common_tools import standard_path
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "../common_tools/forms/ui_empty.ui"))
 
 
-class CreateExampleDialog(QtWidgets.QDialog, FROM_CLASS):
+class CreateExampleDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, qgis_project):
         QtWidgets.QDialog.__init__(self)
         self.iface = qgis_project.iface
@@ -37,16 +35,13 @@ class CreateExampleDialog(QtWidgets.QDialog, FROM_CLASS):
         self.select_coquimbo.setText("Coquimbo")
         self.select_coquimbo.setChecked(False)
 
-        self.place = QLineEdit()
-        self.place.setVisible(False)
-
         self.source_type_frame = QVBoxLayout()
         self.source_type_frame.setAlignment(Qt.AlignLeft)
         self.source_type_frame.addWidget(self.select_sfalls)
         self.source_type_frame.addWidget(self.select_nauru)
         self.source_type_frame.addWidget(self.select_coquimbo)
 
-        self.source_type_widget = QGroupBox("Models")
+        self.source_type_widget = QGroupBox("Available models")
         self.source_type_widget.setLayout(self.source_type_frame)
 
         # Buttons and output
@@ -60,24 +55,45 @@ class CreateExampleDialog(QtWidgets.QDialog, FROM_CLASS):
         self.but_run.setText(self.tr("Create"))
         self.but_run.clicked.connect(self.run)
 
+        self.buttons_frame = QVBoxLayout()
+        self.buttons_frame.addWidget(self.but_choose_output)
+        self.buttons_frame.addWidget(self.output_path)
+        self.buttons_frame.addWidget(self.but_run)
+
+        self.buttons_widget = QWidget()
+        self.buttons_widget.setLayout(self.buttons_frame)
+
+        self.update_widget = QWidget()
+        self.update_frame = QVBoxLayout()
+        self.update_widget.setLayout(self.update_frame)
+        self.update_widget.setVisible(False)
+
+        self._run_layout.addWidget(self.source_type_widget)
+        self._run_layout.addWidget(self.buttons_widget)
+        self._run_layout.addWidget(self.update_widget)
+
+        self.setLayout(self._run_layout)
+        self.resize(280, 250)
+
     
     def choose_output(self):
+        if self.select_sfalls.isChecked():
+            self.place = "sioux_falls"
+        elif self.select_nauru.isChecked():
+            self.place = "nauru"
+        else:
+            self.place = "coquimbo"
+    
         new_name = QFileDialog.getExistingDirectory(QWidget(), "Parent folder", standard_path())
         if new_name is not None and len(new_name) > 0:
-            new_folder = "new_project"
+            new_folder = f"example_{self.place}"
             counter = 1
             while isdir(join(new_name, new_folder)):
-                new_folder = f"new_project_{counter}"
+                new_folder = f"example_{self.place}_{counter}"
                 counter += 1
             self.output_path.setText(join(new_name, new_folder))
 
 
     def run(self):
-        if self.select_sfalls.isChecked():
-            place = "sioux_falls"
-        elif self.select_nauru.isChecked():
-            place = "nauru"
-        else:
-            place = "coquimbo"
 
-        create_example(self.output_path, place)
+        create_example(self.output_path.text(), self.place)
