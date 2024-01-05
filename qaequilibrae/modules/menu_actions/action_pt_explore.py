c2770886df8f0fcb002a7363ec6b1894158daa5d
Renata Imai <renataakemii@yahoo.co.uk>
2024-01-05 12:10:59 -0300

    adds files

--- qaequilibrae/modules/menu_actions/action_pt_explore.py
+++ qaequilibrae/modules/menu_actions/action_pt_explore.py
@@ -1,10 +1,10 @@
-def run_pt_explore(qgisproject):
+def run_pt_explore(qgis_project):
     from qaequilibrae.modules.public_transport_procedures.transit_navigator_dialog import TransitNavigatorDialog
 
-    if qgisproject.project is None:
-        qgisproject.iface.messageBar().pushMessage("Error", "You need to load a project first", level=3, duration=10)
+    if qgis_project.project is None:
+        qgis_project.iface.messageBar().pushMessage("Error", "You need to load a project first", level=3, duration=10)
         return
 
-    dlg2 = TransitNavigatorDialog(qgisproject)
+    dlg2 = TransitNavigatorDialog(qgis_project)
     dlg2.show()
     dlg2.exec_()
