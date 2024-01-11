e4c86750fe564c2baf96550d22ab0c8abf90fe37
Renata Imai <renataakemii@yahoo.co.uk>
2024-01-11 13:56:14 -0300

    fixes imports

--- qaequilibrae/modules/public_transport_procedures/transit_supply_metrics.py
+++ qaequilibrae/modules/public_transport_procedures/transit_supply_metrics.py
@@ -4,7 +4,6 @@ from typing import List, Optional
 import pandas as pd
 import numpy as np
 
-from aequilibrae.transit.constants import WALK_AGENCY_ID as WID
 from aequilibrae.project.database_connection import database_connection
 from aequilibrae.utils.db_utils import read_and_close
 
