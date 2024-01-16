ba79af5161ebd7b7ae353dc9b24f1ab556e54215
Renata Imai <renataakemii@yahoo.co.uk>
2024-01-16 16:42:07 -0300

    .

--- test/test_load_result_table.py
+++ test/test_load_result_table.py
@@ -3,11 +3,9 @@ import pytest
 from qaequilibrae.modules.matrix_procedures.load_result_table import load_result_table
 
 
-@pytest.mark.skip("not working")
-def test_load_result_table(ae_with_project, run_traffic_assignment, qtbot):
+@pytest.mark.skip("Not working")
+def test_load_result_table(ae_with_project):
 
-    run_traffic_assignment(ae_with_project, qtbot, "LTR")
-
-    layer = load_result_table(ae_with_project.project.project_base_path, "TrafficAssignment_DP_LTR")
+    layer = load_result_table(ae_with_project.project.project_base_path, "TrafficAssignment_DP_A")
 
     assert layer.isValid()
