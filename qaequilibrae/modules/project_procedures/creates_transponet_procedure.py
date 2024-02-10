875cfbe5f56d2c625cffe9b18a78f5475c8f5700
pveigadecamargo <pveigadecamargo@anl.gov>
2024-02-10 18:08:55 +1000

    cleans code

--- qaequilibrae/modules/project_procedures/creates_transponet_procedure.py
+++ qaequilibrae/modules/project_procedures/creates_transponet_procedure.py
@@ -1,12 +1,10 @@
-from string import ascii_letters
-
 from PyQt5.QtCore import pyqtSignal
-from aequilibrae.project import Project
-from aequilibrae.utils.db_utils import commit_and_close
-from aequilibrae.project.database_connection import database_connection
-
 from aequilibrae.context import get_logger
+from aequilibrae.project import Project
+from aequilibrae.project.database_connection import database_connection
+from aequilibrae.utils.db_utils import commit_and_close
 from aequilibrae.utils.worker_thread import WorkerThread
+from string import ascii_letters
 
 logger = get_logger()
 
@@ -48,7 +46,7 @@ class CreatesTranspoNetProcedure(WorkerThread):
     # Adds the non-standard fields to a layer
     def additional_fields_to_layers(self, table, layer, layer_fields):
         with commit_and_close(database_connection("network")) as conn:
-                
+
             fields = layer.dataProvider().fields()
             string_fields = []
 
@@ -64,10 +62,7 @@ class CreatesTranspoNetProcedure(WorkerThread):
                     field_type = "char"
                     string_fields.append(f)
                 else:
-                    if "Int" in field.typeName():
-                        field_type = "INTEGER"
-                    else:
-                        field_type = "REAL"
+                    field_type = "INTEGER" if "Int" in field.typeName() else "REAL"
                 try:
                     sql = "alter table " + table + " add column " + f + " " + field_type + "(" + str(field_length) + ")"
                     conn.execute(sql)
@@ -106,7 +101,8 @@ class CreatesTranspoNetProcedure(WorkerThread):
             crs = int(self.node_layer.crs().authid().split(":")[1])
             for j, f in enumerate(self.node_layer.getFeatures()):
                 self.emit_messages(value=j + 1)
-                attrs = [self.convert_data(f.attributes()[val]) if val >= 0 else None for val in self.node_fields.values()]
+                attrs = [self.convert_data(f.attributes()[val]) if val >= 0 else None for val in
+                         self.node_fields.values()]
                 wkb = f.geometry().asWkb().data()
                 node_id = conn.execute(find_sql, [wkb, crs, wkb, crs]).fetchall()
                 if not node_id:
@@ -139,37 +135,7 @@ class CreatesTranspoNetProcedure(WorkerThread):
 
         with commit_and_close(database_connection("network")) as conn:
             if table == "links":
-                # We check if all modes exist
-                modes = self.project.network.modes
-                current_modes = list(modes.all_modes().keys())
-                letters = [x for x in list(ascii_letters) if x not in current_modes]
-                all_modes = [x for x in all_modes if x not in current_modes]
-                for md in all_modes:
-                    new_mode = modes.new(md)
-                    new_mode.mode_name = md
-                    new_mode.description = "Mode automatically added during project creation from layers"
-                    modes.add(new_mode)
-                    new_mode.save()
-                    logger.info(f"{new_mode.description} --> ({md})")
-
-                conn.commit()
-
-                # We check if all link types exist
-                link_types = self.project.network.link_types
-                current_lt = [lt.link_type for lt in link_types.all_types().values()]
-                letters = [x for x in list(ascii_letters) if x not in link_types.all_types().keys()]
-
-                all_link_types = [lt for lt in all_link_types if lt not in current_lt]
-                logger.info(all_link_types)
-                for lt in all_link_types:
-                    new_link_type = link_types.new(letters[0])
-                    letters = letters[1:]
-                    new_link_type.link_type = lt
-                    new_link_type.description = "Link_type automatically added during project creation from layers"
-                    new_link_type.save()
-                    logger.info(new_link_type.description + f" --> ({new_link_type.link_type})")
-
-                conn.commit()
+                self.__add_linktypes_and_modes(all_link_types, all_modes, conn)
 
             for data in data_to_add:
                 try:
@@ -181,16 +147,40 @@ class CreatesTranspoNetProcedure(WorkerThread):
                     if data[0]:
                         msg = self.tr("feature with id {} could not be added to layer {}").format(data[0], table)
                     else:
-                        msg = self.tr("feature with no node id present. It could not be added to layer {}").format(table)
+                        msg = self.tr("feature with no node id present. It could not be added to layer {}").format(
+                            table)
                     self.report.append(msg)
 
-            conn.commit()
+    def __add_linktypes_and_modes(self, all_link_types, all_modes, conn):
+        # We check if all modes exist
+        modes = self.project.network.modes
+        current_modes = list(modes.all_modes().keys())
+        all_modes = [x for x in all_modes if x not in current_modes]
+        for md in all_modes:
+            new_mode = modes.new(md)
+            new_mode.mode_name = md
+            new_mode.description = "Mode automatically added during project creation from layers"
+            modes.add(new_mode)
+            new_mode.save()
+            logger.info(f"{new_mode.description} --> ({md})")
+        conn.commit()
+        # We check if all link types exist
+        link_types = self.project.network.link_types
+        current_lt = [lt.link_type for lt in link_types.all_types().values()]
+        letters = [x for x in list(ascii_letters) if x not in link_types.all_types().keys()]
+        all_link_types = [lt for lt in all_link_types if lt not in current_lt]
+        logger.info(all_link_types)
+        for lt in all_link_types:
+            new_link_type = link_types.new(letters[0])
+            letters = letters[1:]
+            new_link_type.link_type = lt
+            new_link_type.description = "Link_type automatically added during project creation from layers"
+            new_link_type.save()
+            logger.info(new_link_type.description + f" --> ({new_link_type.link_type})")
+        conn.commit()
 
     def convert_data(self, value):
-        if type(value) is None:
-            return None
-        else:
-            return value
+        return None if type(value) is None else value
 
     def emit_messages(self, message="", value=-1, max_val=-1):
         if len(message) > 0:
