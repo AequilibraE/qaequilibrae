2de54d3ee002ef749ec27f75954eb96547130813
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2024-03-22 19:28:50 -0300

    Removes unused files (#306)
    * removes files
    
    * .

--- qaequilibrae/modules/matrix_procedures/__init__.py
+++ qaequilibrae/modules/matrix_procedures/__init__.py
@@ -5,5 +5,3 @@ from .load_matrix_dialog import LoadMatrixDialog
 from .load_project_data import LoadProjectDataDialog
 from .matrix_lister import list_matrices
 from .results_lister import list_results
-
-# from .matrix_manipulation_dialog import MatrixManipulationDialog
