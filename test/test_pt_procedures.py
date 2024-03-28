11cb2f1c1e32eaef83ed35f280c0b960a2cdce39
Renata Imai <renataakemii@yahoo.co.uk>
2024-03-28 12:55:13 -0300

    .

--- test/test_pt_procedures.py
+++ test/test_pt_procedures.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
 import pytest
+from datetime import datetime
+from aequilibrae.transit import Transit
 from PyQt5.QtCore import QTimer
 from qaequilibrae.modules.public_transport_procedures.gtfs_feed import GTFSFeed
 from qaequilibrae.modules.public_transport_procedures.gtfs_importer import GTFSImporter
@@ -19,15 +21,17 @@ def test_pt_menu(ae_with_project, qtbot):
     action.trigger()
 
 
-def test_add_new_feed(pt_no_feed):
-    from aequilibrae.transit import Transit
+def test_add_new_feed(pt_no_feed, mocker):
+    mocker.patch(
+        "qaequilibrae.modules.public_transport_procedures.gtfs_feed.GTFSFeed.open_feed",
+    )
 
     importer = GTFSImporter(pt_no_feed)
     assert importer.label_3.text() == "Add transit table"
     assert importer.rdo_clear.text() == "Create new route system"
 
     data = Transit(pt_no_feed.project)
-    feed = GTFSFeed(pt_no_feed, data, True)
+    feed = GTFSFeed(pt_no_feed, data)
 
     assert feed.label.text() == "Route capacities"
     assert feed.label_2.text() == "Service date"
@@ -38,7 +42,7 @@ def test_add_new_feed(pt_no_feed):
     feed.set_data(gtfs_file)
     feed.led_agency.setText("New agency")
     feed.led_description.setText("Adds new agency description")
-    feed.date = "2016-04-13"
+    feed.service_calendar.setSelectedDate(datetime(2016,4,16))
     feed.return_feed()
 
     importer.set_feed(feed.feed)
@@ -54,10 +58,12 @@ def test_add_new_feed(pt_no_feed):
 
 @pytest.mark.parametrize(
     ("is_checked", "set_date", "set_agency"),
-    [(False, "2016-06-17", "New agency"), (True, "2016-08-21", "Other agency")],
+    [(False, (2016,6,17), "New agency"), (True, (2016,8,21), "Other agency")],
 )
-def test_add_other_feed(pt_project, set_agency, set_date, is_checked):
-    from aequilibrae.transit import Transit
+def test_add_other_feed(pt_project, set_agency, set_date, is_checked, mocker):
+    mocker.patch(
+        "qaequilibrae.modules.public_transport_procedures.gtfs_feed.GTFSFeed.open_feed",
+    )
 
     importer = GTFSImporter(pt_project)
     assert importer.label_3.text() == "Resetting Transit Tables"
@@ -65,7 +71,7 @@ def test_add_other_feed(pt_project, set_agency, set_date, is_checked):
     assert importer.rdo_keep.text() == "Add to Existing Routes"
 
     data = Transit(pt_project.project)
-    feed = GTFSFeed(pt_project, data, True)
+    feed = GTFSFeed(pt_project, data)
 
     assert feed.label.text() == "Route capacities"
     assert feed.label_2.text() == "Service date"
@@ -76,7 +82,7 @@ def test_add_other_feed(pt_project, set_agency, set_date, is_checked):
     feed.set_data(gtfs_file)
     feed.led_agency.setText(set_agency)
     feed.led_description.setText("Adds new agency description")
-    feed.date = set_date
+    feed.service_calendar.setSelectedDate(datetime(*set_date))
     feed.return_feed()
 
     importer.rdo_clear.setChecked(is_checked)
