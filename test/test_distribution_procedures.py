e47d6294856ed52b303996a8eb89f2fa5536ed39
Renata Imai <renataakemii@yahoo.co.uk>
2024-03-28 12:12:17 -0300

    Update test_distribution_procedures.py

--- test/test_distribution_procedures.py
+++ test/test_distribution_procedures.py
@@ -1,5 +1,4 @@
-from os.path import isfile, splitext, basename, join
-from shutil import copy
+from os.path import isfile, splitext, basename
 import numpy as np
 import openmatrix as omx
 import pytest
