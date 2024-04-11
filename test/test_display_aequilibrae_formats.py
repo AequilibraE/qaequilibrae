51f264acea19a61256e000d398c67d1740087d9d
Renata Imai <renataakemii@yahoo.co.uk>
2024-04-11 13:45:05 -0300

    .

---
+++ test/test_display_aequilibrae_formats.py
@@ -0,0 +1,8 @@
+import pytest
+from qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog import DisplayAequilibraEFormatsDialog
+
+
+# TODO:
+@pytest.mark.skip("")
+def test_display_data():
+    dialog = DisplayAequilibraEFormatsDialog()
