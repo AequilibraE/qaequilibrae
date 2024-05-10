59c01752e92df3d2d89b7927068481a85bcb5ed1
Pedro Camargo <c@margo.co>
2024-05-10 14:47:09 -0500

    .

--- ci/installation.py
+++ ci/installation.py
@@ -1,4 +1,10 @@
-from .qaequilibrae.download_extra_packages_class import download_all
+import sys
+from pathlib import Path
+
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from qaequilibrae.download_extra_packages_class import download_all
 
 
 download_all().install(True)
