4b7daa1614acd4eaa356a5ba29e2505de8cd4685
pveigadecamargo <pveigadecamargo@anl.gov>
2024-05-16 09:53:35 -0500

    Fixes install

--- ci/dependency_installation.py
+++ ci/dependency_installation.py
@@ -1,5 +1,10 @@
+import sys
+from pathlib import Path
+
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from qaequilibrae.download_extra_packages_class import DownloadAll
 
 
-on_latest =  sys.version_info >= (3, 12)
+on_latest = sys.version_info >= (3, 12)
 DownloadAll().install(on_latest)
