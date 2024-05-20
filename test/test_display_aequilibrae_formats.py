734a586f972a01e5b98c1aabdd3704cbadbcadd1
Renata Imai <renataakemii@yahoo.co.uk>
2024-05-20 09:30:08 -0300

    .

--- test/test_display_aequilibrae_formats.py
+++ test/test_display_aequilibrae_formats.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pytest
 
+from qgis.core import QgsProject
 from qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog import DisplayAequilibraEFormatsDialog
 
 
@@ -50,12 +51,29 @@ def test_display_data_with_path(tmpdir, ae_with_project, mocker, has_project, pa
 
 
 # Ideally, we would test if the visualization is working
-# @pytest.mark.parametrize("element", [])  # select row or column
-def test_select_elements(ae_with_project, mocker):
+@pytest.mark.parametrize("element", ["row", "column"])
+def test_select_elements(ae_with_project, mocker, element):
     file_path = f"test/data/SiouxFalls_project/matrices/sfalls_skims.omx"
     _, extension = file_path.split(".")
     file_func = "qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog.DisplayAequilibraEFormatsDialog.get_file_name"
     mocker.patch(file_func, return_value=(file_path, extension.upper()))
 
     dialog = DisplayAequilibraEFormatsDialog(ae_with_project, "", True)
-    
+    dialog.no_mapping.setChecked(False)
+    if element == "row":
+        dialog.by_row.setChecked(True)
+        dialog.table.selectRow(0)
+    else:
+        dialog.by_col.setChecked(True)
+        dialog.table.selectColumn(0)
+
+    # For both columns and rows
+    existing_layers = [vector.name() for vector in QgsProject.instance().mapLayers().values()]
+    assert "matrix_row" in existing_layers
+
+    layer = QgsProject.instance().mapLayersByName("zones")[0]
+    field_names = [field.name() for field in layer.fields()]
+    assert "metrics_data" in field_names
+
+    metrics = [round(feat["metrics_data"], 0) for feat in layer.getFeatures()]
+    assert metrics == [0.0, 6.0, 4.0]
