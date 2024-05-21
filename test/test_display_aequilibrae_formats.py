697e6269b8905e33317da40d7c162731165c0272
Pedro Camargo <c@margo.co>
2024-05-21 23:35:37 +1000

    Maps matrices (#335)

--- test/test_display_aequilibrae_formats.py
+++ test/test_display_aequilibrae_formats.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pytest
 
+from qgis.core import QgsProject
 from qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog import DisplayAequilibraEFormatsDialog
 
 
@@ -13,19 +14,20 @@ def test_display_data_no_path(ae, mocker):
     dialog.close()
 
     messagebar = ae.iface.messageBar()
-    assert (
-        messagebar.messages[1][-1] == "Error::Path provided is not a valid dataset"
-    ), "Level 1 error message is missing"
+    error_message = "Error::Path provided is not a valid dataset"
+    assert messagebar.messages[1][-1] == error_message, "Level 1 error message is missing"
 
 
 @pytest.mark.parametrize("has_project", [True, False])
-@pytest.mark.parametrize("file_name", ("demand.aem", "SiouxFalls.omx"))
-def test_display_data_with_path(tmpdir, ae_with_project, mocker, has_project, file_name):
-    file_path = f"test/data/SiouxFalls_project/matrices/{file_name}"
-    name, extension = file_name.split(".")
+@pytest.mark.parametrize("path", ("matrices/demand.aem", "matrices/SiouxFalls.omx", "synthetic_future_vector.aed"))
+def test_display_data_with_path(tmpdir, ae_with_project, mocker, has_project, path):
+    file_path = f"test/data/SiouxFalls_project/{path}"
+    name, extension = path.split(".")
     file_func = "qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog.DisplayAequilibraEFormatsDialog.get_file_name"
     mocker.patch(file_func, return_value=(file_path, extension.upper()))
 
+    if "/" in name:
+        _, name = name.split("/")
     out_func = "qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog.DisplayAequilibraEFormatsDialog.csv_file_path"
     mocker.patch(out_func, return_value=f"{tmpdir}/{name}.csv")
 
@@ -33,9 +35,50 @@ def test_display_data_with_path(tmpdir, ae_with_project, mocker, has_project, fi
     dialog.export()
     dialog.exit_procedure()
 
+    if extension in ["aem", "omx"]:
+        assert np.sum(dialog.data_to_show.matrix["matrix"]) == 360600
+        assert "matrix" in dialog.list_cores
+        assert "taz" in dialog.list_indices
+    elif extension == "aed":
+        assert dialog.list_cores == ["origins", "destinations"]
+        assert sum(dialog.data_to_show.data["origins"]) == 436740
+        assert sum(dialog.data_to_show.data["destinations"]) == 436740
+
     assert dialog.error is None
-    assert np.sum(dialog.data_to_show.__dict__["matrix"]["matrix"]) == 360600
-    assert "matrix" in dialog.list_cores
-    assert "taz" in dialog.list_indices
     assert dialog.data_type == extension.upper()
     assert os.path.isfile(f"{tmpdir}/{name}.csv")
+
+
+# TODO: Ideally, we would test if the visualization is working
+@pytest.mark.parametrize("element", ["row", "column"])
+def test_select_elements(ae_with_project, mocker, element):
+    file_path = "test/data/SiouxFalls_project/matrices/sfalls_skims.omx"
+    _, extension = file_path.split(".")
+    file_func = "qaequilibrae.modules.matrix_procedures.display_aequilibrae_formats_dialog.DisplayAequilibraEFormatsDialog.get_file_name"
+    mocker.patch(file_func, return_value=(file_path, extension.upper()))
+
+    dialog = DisplayAequilibraEFormatsDialog(ae_with_project, "", True)
+    dialog.no_mapping.setChecked(False)
+    if element == "row":
+        dialog.by_row.setChecked(True)
+        dialog.table.selectRow(0)
+    else:
+        dialog.by_col.setChecked(True)
+        dialog.table.selectColumn(0)
+
+    dialog.exit_procedure()
+
+    # For both columns and rows
+    existing_layers = [vector.name() for vector in QgsProject.instance().mapLayers().values()]
+    assert "matrix_row" in existing_layers
+
+    layer = QgsProject.instance().mapLayersByName("zones")[0]
+    field_names = [field.name() for field in layer.fields()]
+    assert "metrics_data" in field_names
+
+    metrics = [round(feat["metrics_data"], 0) for feat in layer.getFeatures()]
+    assert metrics == [0.0, 6.0, 4.0]
+
+    QgsProject.instance().removeAllMapLayers()
+
+    dialog.omx.close()
