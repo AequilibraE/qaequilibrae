5663965227864cd562e3a3e68520c8d8047b96b0
Arthur Evrard <83211842+Art-Ev@users.noreply.github.com>
2024-05-24 16:05:47 +0200

    Change provider name to AequilibraE

--- qaequilibrae/modules/processing_provider/provider.py
+++ qaequilibrae/modules/processing_provider/provider.py
@@ -34,14 +34,14 @@ class Provider(QgsProcessingProvider):
         This string should be a unique, short, character only string,
         eg "qgis" or "gdal". This string should not be localised.
         """
-        return 'qAequilibraE'
+        return 'AequilibraE'
 
     def name(self):
         """The human friendly name of the plugin in Processing.
         This string should be as short as possible (e.g. "Lastools", not
         "Lastools version 1.0.1 64-bit") and localised.
         """
-        return self.tr('qAequilibraE')
+        return self.tr('AequilibraE')
 
     def icon(self):
         """Should return a QIcon which is used for your provider inside
