add42b9aefc2698a12a655cad0659ccb3a21e650
Renata Imai <renataakemii@yahoo.co.uk>
2024-10-01 08:52:44 -0300

    add test

--- test/test_simple_tag.py
+++ test/test_simple_tag.py
@@ -1,11 +1,107 @@
+import sqlite3
+from pathlib import Path
+from os.path import join
 import pytest
+from aequilibrae.utils.db_utils import commit_and_close
+from aequilibrae.project.database_connection import database_connection
+from qgis.core import QgsProject, QgsVectorLayer, QgsField, QgsFeature
+from qgis.core import QgsFeature, QgsPointXY, QgsGeometry
+from qgis.PyQt.QtCore import QVariant
 
 from qaequilibrae.modules.gis.simple_tag_dialog import SimpleTagDialog
 from qaequilibrae.modules.gis.simple_tag_procedure import SimpleTAG
 
 
-@pytest.mark.skip
-def test_simple_tag(pt_project):
-    dialog = SimpleTagDialog(pt_project)
+def create_nodes_layer():
+    layer = QgsVectorLayer("Point?crs=epsg:4326", "Centroids", "memory")
+    if not layer.isValid():
+        print("Nodes layer failed to load!")
+    else:
+        field_id = QgsField("ID", QVariant.Int)
+        field_zone_id = QgsField("zone_id", QVariant.Int)
+        nickname = QgsField("name", QVariant.String)
 
-    print(dialog.__dict__)
+        layer.dataProvider().addAttributes([field_id])
+        layer.dataProvider().addAttributes([field_zone_id])
+        layer.dataProvider().addAttributes([nickname])
+        layer.updateFields()
+
+        points = [
+            QgsPointXY(-71.2489, -29.8936),
+            QgsPointXY(-71.2355, -29.8947),
+            QgsPointXY(-71.2350, -29.8875),
+        ]
+
+        attributes = ([97, 98, 99], [None, None, None])
+
+        features = []
+        for i, (point, zone_id, name) in enumerate(zip(points, *attributes)):
+            feature = QgsFeature()
+            feature.setGeometry(QgsGeometry.fromPointXY(point))
+            feature.setAttributes([i + 1, zone_id, name])
+            features.append(feature)
+
+        layer.dataProvider().addFeatures(features)
+
+        QgsProject.instance().addMapLayer(layer)
+
+    return layer
+
+
+@pytest.mark.parametrize("ops", ["ENCLOSED", "CLOSEST"])
+def test_simple_tag_polygon_and_point(coquimbo_project, ops):
+    # Load zoning layer
+    coquimbo_project.load_layer_by_name("zones")
+
+    cities = ["Valparaiso", "Santiago", "Antofagasta"]
+    with commit_and_close(database_connection("network")) as conn:
+        for i, zone in enumerate([97, 98, 99]):
+            conn.execute(f"UPDATE zones SET name='{cities[i]}' WHERE zone_id={zone}")
+
+    nodes_layer = create_nodes_layer()
+
+    prj_layers = [lyr.name() for lyr in QgsProject.instance().mapLayers().values()]
+    assert "Centroids" in prj_layers
+    assert "zones" in prj_layers
+
+    dialog = SimpleTagDialog(coquimbo_project)
+
+    dialog.fromlayer.setCurrentText("zones")
+    dialog.fromfield.setCurrentIndex(3)
+    dialog.tolayer.setCurrentText("Centroids")
+    dialog.tofield.setCurrentIndex(2)
+
+    dialog.set_from_fields()
+    dialog.set_to_fields()
+    dialog.set_available_operations()
+
+    dialog.worker_thread = SimpleTAG(
+        parentThread=coquimbo_project.iface.mainWindow(),
+        flayer="zones",
+        ffield="name",
+        tlayer="Centroids",
+        tfield="name",
+        fmatch=None,
+        tmatch=None,
+        operation=ops,
+        geo_types=dialog.geography_types,
+    )
+    dialog.worker_thread.doWork()
+
+    # zones = join(coquimbo_project.project.project_base_path, "project_database.sqlite")
+    # con = sqlite3.connect(zones)
+
+    # print(con.execute("SELECT zone_id, name, population FROM zones WHERE zone_id IN (97, 98, 99)").fetchall())
+
+    feats = [f["name"] for f in nodes_layer.getFeatures()]
+    assert feats == cities
+
+    QgsProject.instance().clear()
+
+
+def test_simple_tag_polygon_and_linestring():
+    pass
+
+
+def test_simple_tag_linestring_and_point():
+    pass
