059acb170c907f0fc58dbac8c13eb6e989cd0850
Renata Imai <renataakemii@yahoo.co.uk>
2025-02-28 13:09:25 -0300

    Update pandas_model.py

--- qaequilibrae/modules/common_tools/pandas_model.py
+++ qaequilibrae/modules/common_tools/pandas_model.py
@@ -1,6 +1,14 @@
-"""From https://stackoverflow.com/a/31557937/1480643"""
+"""
+This class's first version was from https://stackoverflow.com/a/31557937/1480643
+and was updated using Matrin Fizpatrick's "Create GUI applications with Python
+and PyQt5" code. Please see page 341 for further reference.
+
+According to the author, the code examples in the book are free to use without 
+license.
+"""
 
 from qgis.PyQt import QtCore
+from PyQt5.QtCore import Qt
 
 
 class PandasModel(QtCore.QAbstractTableModel):
@@ -12,19 +20,21 @@ class PandasModel(QtCore.QAbstractTableModel):
         QtCore.QAbstractTableModel.__init__(self, parent)
         self._data = data
 
-    def rowCount(self, parent=None):
-        return len(self._data.values)
-
-    def columnCount(self, parent=None):
-        return self._data.columns.size
-
-    def data(self, index, role=QtCore.Qt.DisplayRole):
+    def data(self, index, role):
         if index.isValid():
-            if role == QtCore.Qt.DisplayRole:
-                return str(self._data.iloc[index.row()][index.column()])
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
         return None
 
-    def header_data(self, col, orientation, role):
-        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
-            return self._data.columns[col]
-        return None
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def header_data(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
