877ba6e92017a296105b8de30655971db6e249e0
Renata Imai <renataakemii@yahoo.co.uk>
2025-04-30 10:30:35 -0300

    Update load_result_table.py

--- qaequilibrae/modules/matrix_procedures/load_result_table.py
+++ qaequilibrae/modules/matrix_procedures/load_result_table.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import sqlite3
 from os.path import join
 
 from qgis.utils import spatialite_connect
@@ -6,15 +8,19 @@ from qgis._core import QgsProject, QgsVectorLayer, QgsDataSourceUri
 
 def load_result_table(project_base_path: str, table_name: str) -> QgsVectorLayer:
     pth = join(project_base_path, "results_database.sqlite")
-    conn = spatialite_connect(pth)
-    conn.execute("PRAGMA temp_store = 0;")
-    conn.execute("SELECT InitSpatialMetaData();")
-    conn.commit()
-    conn.close()
+    conn = sqlite3.connect(pth)
 
-    uri = QgsDataSourceUri()
-    uri.setDatabase(pth)
-    uri.setDataSource("", table_name, None)
-    lyr = QgsVectorLayer(uri.uri(), table_name, "spatialite")
-    QgsProject.instance().addMapLayer(lyr)
-    return lyr
+    return pd.read_sql(f"select * from {table_name};", con=conn)
+    # conn = spatialite_connect(pth)
+    # conn.execute("PRAGMA temp_store = 0;")
+    # conn.execute("SELECT InitSpatialMetaData();")
+    # conn.commit()
+    # conn.close()
+
+    # uri = QgsDataSourceUri()
+    # uri.setDatabase(pth)
+    # uri.setDataSource("", table_name, None)
+    # lyr = QgsVectorLayer(uri.uri(), table_name, "spatialite")
+    # QgsProject.instance().addMapLayer(lyr)
+    # return lyr
+    
