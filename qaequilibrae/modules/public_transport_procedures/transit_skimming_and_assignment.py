e180d89a4a3439a12ae411a3a0727884c3955fcb
Renata Imai <renataakemii@yahoo.co.uk>
2025-05-07 15:25:19 -0300

    Populate UI and write test

--- qaequilibrae/modules/public_transport_procedures/transit_skimming_and_assignment.py
+++ qaequilibrae/modules/public_transport_procedures/transit_skimming_and_assignment.py
@@ -4,7 +4,9 @@ from aequilibrae.paths import TransitAssignment, TransitClass
 from aequilibrae.transit import Transit
 from aequilibrae.transit.transit_graph_builder import TransitGraphBuilder
 from qgis.PyQt import uic, QtGui
-from qgis.PyQt.QtWidgets import QDialog
+from qgis.PyQt.QtWidgets import QDialog, QTableWidgetItem, QAbstractItemView
+
+from qaequilibrae.modules.matrix_procedures import list_matrices
 
 FORM_CLASS, _ = uic.loadUiType(join(dirname(__file__), "forms/ui_skimming_assignment.ui"))
 
@@ -16,12 +18,17 @@ class TransitSkimAssign(QDialog, FORM_CLASS):
         self.project = qgis_project.project
         self.transit_data = Transit(self.project)
 
+        self.all_modes = {}
+        self.proj_matrices = list_matrices(self.project.matrices.fldr)
+
         self.__populate_project_info()
 
         self.cob_conn_methods.addItems(["Overlapping regions", "Nearest neighbour"])
         self.cob_line_methods.addItems(["Direct", "Connector project match"])
+        self.cob_matrices.currentIndexChanged.connect(self.change_matrix_data)
 
     def __populate_project_info(self):
+        # Add modes
         with self.project.db_connection as conn:
             res = conn.execute("""select mode_name, mode_id from modes""")
 
@@ -33,6 +40,31 @@ class TransitSkimAssign(QDialog, FORM_CLASS):
         self.cob_mode.clear()
         for m in modes:
             self.cob_mode.addItem(m)
+
+        # Add skimming data
+        self.skimmeable_fields = [
+            "boardings",
+            "alightings",
+            "inner_transfers",
+            "outer_transfers",
+            "transfers",
+            "trav_time",
+            "on_board_trav_time",
+            "dwelling_time",
+            "egress_trav_time",
+            "access_trav_time",
+            "walking_trav_time",
+            "transfer_time",
+            "in_vehicle_trav_time",
+            "waiting_time",
+        ]
+        self.available_skims_table.setRowCount(len(self.skimmeable_fields))
+        for i, q in enumerate(self.skimmeable_fields):
+            self.available_skims_table.setItem(i, 0, QTableWidgetItem(q))
+
+        # Add travel time
+
+        # Add frequency
 
     def build_graph(self):
         c_method = self.__get_connector_method()
@@ -63,3 +95,12 @@ class TransitSkimAssign(QDialog, FORM_CLASS):
             return "overlapping_regions"
         else:
             return "nearest_neighbour"
+
+    def change_matrix_data(self):
+        self.cob_matrix_core.clear()
+
+        file_name = self.proj_matrices.at[self.cob_matrices.currentIndex(), "file_name"]
+
+        mat = self.proj_matrices.get_matrix(file_name)
+
+        self.cob_matrix_core.addItems(mat.names)
---
+++ test/test_pt_assignment.py
@@ -0,0 +1,24 @@
+from os.path import join
+import numpy as np
+
+from qaequilibrae.modules.public_transport_procedures.transit_skimming_and_assignment import TransitSkimAssign
+from .utilities import create_matrix
+
+
+def create_dialog_with_matrix(project):
+    pth = join(project.project.project_base_path, "matrices/demand.aem")
+    create_matrix(np.arange(1, 134), pth)
+
+    matrices = project.project.matrices
+    matrices.update_database()
+    matrices.reload()
+
+    return TransitSkimAssign(project)
+
+
+def test_init(qtbot, coquimbo_project):
+    dialog = create_dialog_with_matrix(coquimbo_project)
+
+    for i in [0, 1]:
+        path = qtbot.screenshot(dialog.tabWidget.widget(i), suffix=f"{i}")
+        print(path)
