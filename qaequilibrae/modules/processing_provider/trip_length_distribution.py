5b9509d4e1e7a825f5cb79f774caaa47863f6622
Renata Imai <renataakemii@yahoo.co.uk>
2025-05-15 17:21:13 -0300

    Update trip_length_distribution.py

--- qaequilibrae/modules/processing_provider/trip_length_distribution.py
+++ qaequilibrae/modules/processing_provider/trip_length_distribution.py
@@ -1,30 +1,43 @@
-from typing import Any, Dict
+import importlib.util as iutil
+import sys
+
 from qgis._core import QgsProcessingContext, QgsProcessingFeedback
 from qgis.core import QgsProcessingAlgorithm
 
 from qaequilibrae.i18n.translate import trlt
 
+
 class TripLengthDistribution(QgsProcessingAlgorithm):
-    def initAlgorithm(self, configuration: Dict[str, Any] = ...) -> None:
-        return super().initAlgorithm(configuration)
-    
-    def processAlgorithm(self, parameters: Dict[str, Any], context: QgsProcessingContext, feedback: QgsProcessingFeedback | None) -> Dict[str, Any]:
-        return super().processAlgorithm(parameters, context, feedback)
-    
+    def initAlgorithm(self, config=None):
+        self.addParameter()
+
+    def processAlgorithm(self, parameters, context, feedback):
+        # Checks if we have AequilibraE installed
+        if iutil.find_spec("aequilibrae") is None:
+            sys.exit(self.tr("AequilibraE module not found"))
+
+        from aequilibrae.matrix import AequilibraeMatrix
+        import matplotlib.pyplot as plt
+
+        return {"Output": "DONE!"}
+
     def name(self):
         return self.tr("Trip length distribution")
-    
+
     def displayName(self) -> str:
         return self.tr("Trip length distribution")
-    
+
     def group(self) -> str:
         return super().group()
-    
+
     def groupId(self) -> str:
         return super().groupId()
-    
-    def createInstance(self) -> QgsProcessingAlgorithm | None:
-        return super().createInstance()
-    
+
+    def shortHelpString(self):
+        return self.tr("Creates a trip-length distribution histogram and save in an output folder.")
+
+    def createInstance(self):
+        return TripLengthDistribution()
+
     def tr(self, message):
         return trlt("TripLengthDistribution", message)
