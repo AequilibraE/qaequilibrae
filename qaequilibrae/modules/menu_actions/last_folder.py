86855f5e2ccdb5c9d39d91f8a1095d0c9d6807cd
Renata Imai <renataakemii@yahoo.co.uk>
2025-05-29 10:02:25 -0300

    Update last_folder.py

--- qaequilibrae/modules/menu_actions/last_folder.py
+++ qaequilibrae/modules/menu_actions/last_folder.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2025, UChicago Argonne, LLC
-# BSD OPEN SOURCE LICENSE. Full license can be found in LICENSE
 import logging
 import os
 from tempfile import gettempdir
 
 
 def last_folder():
+    """Reads the last-used folder path and its return value.
+    If the folder does not exist or cannot be find, function returns a temporary directory."""
     pth = os.path.join(gettempdir(), "aequilibrae_last_folder.txt")
     if not os.path.isfile(pth):
         with open(pth, "w") as file:
