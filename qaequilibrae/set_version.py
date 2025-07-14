0a98bc788dfd53c1d3c1a7962bebd68ab7168bef
Pedro Camargo <c@margo.co>
2025-07-14 20:51:06 +1000

    Apply suggestions from code review

--- qaequilibrae/set_version.py
+++ qaequilibrae/set_version.py
@@ -11,7 +11,7 @@ if str(project_dir) not in sys.path:
 def set_version(sha):
     # Add missing info to the metadata
     current_time = datetime.now()
-    current_version = current_time.strftime("%y.%j.%f")
+    current_version = current_time.strftime("%y.%j.%H")
 
     metadata_path = project_dir / "qaequilibrae" / "metadata.txt"
     with open(metadata_path, mode="a") as file:
