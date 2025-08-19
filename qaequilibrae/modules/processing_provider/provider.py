5894cddd3416dbbcdc896554b0c61539889d7335
Renata Imai <renataakemii@yahoo.co.uk>
2025-08-19 09:39:05 -0300

    file location

--- qaequilibrae/modules/processing_provider/provider.py
+++ qaequilibrae/modules/processing_provider/provider.py
@@ -15,58 +15,75 @@ if str(provider_path) not in sys.path:
 class Provider(QgsProcessingProvider):
 
     def loadAlgorithms(self):
+
+        self.__load_matrix_procedures()
+        self.__load_network()
+        self.__load_paths_procedures()
+        self.__load_project_procedures()
+        self.__load_public_transport_procedures()
+
+    def __load_matrix_procedures(self):
+
+        from .add_matrix_from_layer import AddMatrixFromLayer
+        from .create_matrix_from_layer import CreateMatrixFromLayer
+        from .export_matrix import ExportMatrix
+        from .matrix_calculator import MatrixCalculator
+
+        self.addAlgorithm(AddMatrixFromLayer())
+        self.addAlgorithm(CreateMatrixFromLayer())
+        self.addAlgorithm(ExportMatrix())
+        self.addAlgorithm(MatrixCalculator())
+
+    def __load_network(self):
+
         from .Add_connectors import AddConnectors
         from .add_links_from_layer import AddLinksFromLayer
-        from .add_matrix_from_layer import AddMatrixFromLayer
-        from .assign_transit_from_yaml import TransitAssignYAML
-        from .assign_traffic_from_yaml import TrafficAssignYAML
-        from .create_matrix_from_layer import CreateMatrixFromLayer
-        from .create_transit_graph import CreatePTGraph
-        from .export_matrix import ExportMatrix
-        from .import_gtfs import ImportGTFS
-        from .matrix_calculator import MatrixCalculator
-        from .project_from_layer import ProjectFromLayer
-        from .project_from_OSM import ProjectFromOSM
-        from .renumber_nodes_from_layer import RenumberNodesFromLayer
-        from .trip_length_distribution import TripLengthDistribution
         from .network_simplifier import NetworkSimplifier
         from .collapse_links import CollapseLinks
-        from .run_module import RunProcedures
+        from .renumber_nodes_from_layer import RenumberNodesFromLayer
+        from .trip_length_distribution import TripLengthDistribution
 
         self.addAlgorithm(AddConnectors())
         self.addAlgorithm(AddLinksFromLayer())
-        self.addAlgorithm(AddMatrixFromLayer())
-        self.addAlgorithm(TransitAssignYAML())
-        self.addAlgorithm(TrafficAssignYAML())
-        self.addAlgorithm(CreateMatrixFromLayer())
-        self.addAlgorithm(CreatePTGraph())
-        self.addAlgorithm(ExportMatrix())
-        self.addAlgorithm(ImportGTFS())
-        self.addAlgorithm(MatrixCalculator())
-        self.addAlgorithm(ProjectFromLayer())
-        self.addAlgorithm(ProjectFromOSM())
+        self.addAlgorithm(CollapseLinks())
+        self.addAlgorithm(NetworkSimplifier())
         self.addAlgorithm(RenumberNodesFromLayer())
         self.addAlgorithm(TripLengthDistribution())
-        self.addAlgorithm(NetworkSimplifier())
-        self.addAlgorithm(CollapseLinks())
+
+    def __load_paths_procedures(self):
+
+        from .assign_traffic_from_yaml import TrafficAssignYAML
+
+        self.addAlgorithm(TrafficAssignYAML())
+
+    def __load_project_procedures(self):
+
+        from .project_from_layer import ProjectFromLayer
+        from .project_from_OSM import ProjectFromOSM
+        from .run_module import RunProcedures
+
+        self.addAlgorithm(ProjectFromLayer())
+        self.addAlgorithm(ProjectFromOSM())
         self.addAlgorithm(RunProcedures())
 
+    def __load_public_transport_procedures(self):
+
+        from .assign_transit_from_yaml import TransitAssignYAML
+        from .create_transit_graph import CreatePTGraph
+        from .import_gtfs import ImportGTFS
+
+        self.addAlgorithm(TransitAssignYAML())
+        self.addAlgorithm(CreatePTGraph())
+        self.addAlgorithm(ImportGTFS())
+
     def id(self):
-        """The ID used for identifying the provider.
-        This string should be a unique, short, character only string,
-        eg "qgis" or "gdal". This string should not be localised.
-        """
-        return "aequilibrae"
+        """The ID used for identifying the provider."""
+        return "qaequilibrae"
 
     def name(self):
-        """The human friendly name of the plugin in Processing.
-        This string should be as short as possible (e.g. "Lastools", not
-        "Lastools version 1.0.1 64-bit") and localised.
-        """
-        return "AequilibraE"
+        """The human friendly name of the plugin in Processing."""
+        return "QAequilibraE"
 
     def icon(self):
-        """Should return a QIcon which is used for your provider inside
-        the Processing toolbox.
-        """
+        """SQIcon used for the provider inside the Processing toolbox."""
         return QIcon(join(provider_path, "icon.png"))
