fac566d9d50ba916f9e9b15e6cc62ae88127c520
Renata Imai <renataakemii@yahoo.co.uk>
2025-08-27 12:36:43 -0300

    rename file

--- qaequilibrae/modules/project_procedures/create_scenarios_dialog.py
+++ qaequilibrae/modules/project_procedures/create_scenarios_dialog.py
@@ -17,13 +17,37 @@ class CreateScenariosDialog(QtWidgets.QDialog, FORM_CLASS):
 
         self.resize(180, 100)
 
-        self.rdo_create.clicked.connect(self.configure_inputs)
-        self.rdo_clone.clicked.connect(self.configure_inputs)
+        self.rdo_create.toggled.connect(self.configure_inputs)
+        self.rdo_clone.toggled.connect(self.configure_inputs)
 
         self.but_run.clicked.connect(self.run)
 
+        self.populate_scenarios()
+
     def configure_inputs(self):
+        """Update UI elements based on selected mode"""
+        if self.rdo_clone.isChecked():
+            # Clone mode: show combo box and change label
+            self.label_1.setText("Scenario to clone")
+            self.txt_name.setVisible(False)
+            self.cob_scenarios.setVisible(True)
+        else:
+            # Create mode: show line edit and change label
+            self.label_1.setText("Scenario name")
+            self.txt_name.setVisible(True)
+            self.cob_scenarios.setVisible(False)
+
+        self.txt_desc.clear()
         self.resize(610, 122)
+
+        # TODO: disable run button if txt_name is empty.
+
+    def populate_scenarios(self):
+        """Populate the combo box with available scenarios"""
+        scenarios = self.project.list_scenarios()["scenario_name"].tolist()
+        
+        self.cob_scenarios.clear()
+        self.cob_scenarios.addItems(scenarios)
 
     def run(self):
         name = self.txt_name.text()
