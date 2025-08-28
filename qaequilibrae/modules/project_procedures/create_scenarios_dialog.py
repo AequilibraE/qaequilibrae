ed50ea4ece938331e54adb2a2f9a0ca7eeec4ad5
Renata Imai <renataakemii@yahoo.co.uk>
2025-08-28 09:06:43 -0300

    scenarios dialog

--- qaequilibrae/modules/project_procedures/create_scenarios_dialog.py
+++ qaequilibrae/modules/project_procedures/create_scenarios_dialog.py
@@ -12,14 +12,10 @@ class CreateScenariosDialog(QtWidgets.QDialog, FORM_CLASS):
         self.project = qgis_project.project
         self.setupUi(self)
 
-        # self.__init_scenario = self.qgis_project.cob_scenarios.currentText()
-
-        self.label_1.setVisible(False)
-        self.cob_scenarios.setVisible(False)
+        self.__init_scenario = self.qgis_project.cob_scenarios.currentText()
 
         self.rdo_create.clicked.connect(self.configure_inputs)
         self.rdo_clone.clicked.connect(self.configure_inputs)
-
         self.but_run.clicked.connect(self.run)
 
         self.populate_scenarios()
@@ -40,38 +36,34 @@ class CreateScenariosDialog(QtWidgets.QDialog, FORM_CLASS):
         """Populate the combo box with available scenarios. The current text displayed refers to the active scenario."""
         self.cob_scenarios.clear()
         self.cob_scenarios.addItems(self.qgis_project.available_scenarios)
-        self.cob_scenarios.setCurrentText(self.qgis_project.cob_scenarios.currentText())
+        self.cob_scenarios.setCurrentText(self.__init_scenario)
 
     def run(self):
         name = self.txt_name.text()
         desc = self.txt_desc.text()
 
         if self.rdo_clone.isChecked():
-            # Aqui precisamos de uma lógica melhor para que o cenário a ser clonado mude
-            # caso não seja o cenário ativo. E esse cenário deve "voltar" ao que era antes
-            # ou seja, o valor mostrado em qgis_project.cob_scenarios não pode mudar, mas
-            # o novo cenário deve ser clonado a partir do cenário correto.
+            if self.__init_scenario != self.cob_scenarios.currentText():
+                self.project.use_scenario(self.cob_scenarios.currentText())
+                txt = f"Changing scenario from {self.__init_scenario} to {self.cob_scenarios.currentText()}"
+                self.qgis_project.log_message(txt)
 
-            # if self.__init_scenario != self.cob_scenarios.currentText():
-            #     self.project.use_scenario(self.cob_scenarios.currentText())
-            #     QgsMessageLog.logMessage(f"Changing scenario from {self.__init_scenario} to {self.cob_scenarios.currentText()}", "Messages", Qgis.Info, False)
-
-            # QgsMessageLog.logMessage(
-            #     f"Scenario to be used: {self.cob_scenarios.currentText()}", "Messages", Qgis.Info, False
-            # )
-            # self.project.clone_scenario(name, desc)
-
-            # self.project.use_scenario(self.__init_scenario)
-            # QgsMessageLog.logMessage(f"Scenario at menu: {self.__init_scenario}", "Messages", Qgis.Info, False)
             self.project.clone_scenario(name, desc)
-            self.qgis_project.log_message(f"Cloned 'root' scenario. New scenario {name} addeed to the project")
+            txt = f"Cloned '{self.cob_scenarios.currentText()}'. New scenario '{name}' addeed to the project"
+            self.qgis_project.log_message(txt)
+
+            if self.__init_scenario != self.cob_scenarios.currentText():
+                self.project.use_scenario(self.__init_scenario)
+                txt = f"Changing scenario from {self.cob_scenarios.currentText()} to {self.__init_scenario}"
+                self.qgis_project.log_message(txt)
         else:
             self.project.create_empty_scenario(name, desc)
-            self.qgis_project.log_message(f"Created empty scenario. New scenario {name} addeed to the project")
+            self.qgis_project.log_message(f"Created empty scenario. New scenario '{name}' addeed to the project")
 
         # Update project scenarios
         self.qgis_project.cob_scenarios.clear()
         self.qgis_project.available_scenarios.extend([name])
         self.qgis_project.cob_scenarios.addItems(self.qgis_project.available_scenarios)
+        self.qgis_project.cob_scenarios.setCurrentText(self.__init_scenario)
 
         self.close()
