a8a798a0bd09dee0554e29a239603328cfcaa204
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2026-06-05 11:20:47 -0300

    fix imports and lint errors

--- qaequilibrae/modules/transit_procedures/__init__.py
+++ qaequilibrae/modules/transit_procedures/__init__.py
@@ -1,5 +1,5 @@
-from .gtfs_feed import GTFSFeed
-from .gtfs_importer import GTFSImporter
-from .transit_navigator_dialog import TransitNavigatorDialog
-from .transit_assignment_dialog import TransitAssignDialog
-from .new_period_dialog import NewPeriodDialog
+from .gtfs_feed import GTFSFeed as GTFSFeed
+from .gtfs_importer import GTFSImporter as GTFSImporter
+from .transit_navigator_dialog import TransitNavigatorDialog as TransitNavigatorDialog
+from .transit_assignment_dialog import TransitAssignDialog as TransitAssignDialog
+from .new_period_dialog import NewPeriodDialog as NewPeriodDialog
